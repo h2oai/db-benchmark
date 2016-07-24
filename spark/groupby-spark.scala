d59e8d5b29394546b4fe2fd13619d11e20a85942
jangorecki <j.gorecki@wit.edu.pl>
2016-07-23 18:43:54 -0700

    spark groupby

---
+++ spark/groupby-spark.scala
@@ -0,0 +1,202 @@
+
+println("# groupby-spark.scala")
+
+import org.apache.spark.sql.SQLContext
+import org.joda.time._
+
+:load ./helpers.scala
+
+val sqlContext = SQLContext.getOrCreate(sc)
+val task = "groupby"
+val ver = sc.version
+val solution = "spark"
+
+/** load data */
+
+val src_grp = sys.env("SRC_GRP")
+val data_name = get_data_name(List(src_grp)) /** paste(basename(.) collapse="-") */
+val t_start = DateTime.now()
+val X = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load(src_grp)
+new Duration(t_start, DateTime.now())
+
+val t_start = DateTime.now()
+X.cache
+new Duration(t_start, DateTime.now())
+
+val t_start = DateTime.now() 
+val in_rows = X.count
+new Duration(t_start, DateTime.now())
+
+/** groupby */
+
+val question = "sum v1 by id1" /** #1 */
+val fun = ".groupBy sum"
+System.gc()
+val ans = X.groupBy("id1").sum("v1")
+val t_start = DateTime.now()
+ans.cache
+val out_rows = ans.count
+val t_end = DateTime.now()
+val t = new Duration(t_start, t_end).getMillis.toDouble / 1000.toDouble
+val m = Double.NaN
+write_log(task=task, data=data_name, in_rows=in_rows, question=question, out_rows=out_rows, solution=solution, version=ver, fun=fun, run=1:Int, time_sec=t, mem_gb=m)
+ans.unpersist(blocking=true)
+System.gc()
+val ans = X.groupBy("id1").sum("v1")
+val t_start = DateTime.now()
+ans.cache
+val out_rows = ans.count
+val t_end = DateTime.now()
+val t = new Duration(t_start, t_end).getMillis.toDouble / 1000.toDouble
+val m = Double.NaN
+write_log(task=task, data=data_name, in_rows=in_rows, question=question, out_rows=out_rows, solution=solution, version=ver, fun=fun, run=2:Int, time_sec=t, mem_gb=m)
+ans.unpersist(blocking=true)
+System.gc()
+val ans = X.groupBy("id1").sum("v1")
+val t_start = DateTime.now()
+ans.cache
+val out_rows = ans.count
+val t_end = DateTime.now()
+val t = new Duration(t_start, t_end).getMillis.toDouble / 1000.toDouble
+val m = Double.NaN
+write_log(task=task, data=data_name, in_rows=in_rows, question=question, out_rows=out_rows, solution=solution, version=ver, fun=fun, run=3:Int, time_sec=t, mem_gb=m)
+ans.unpersist(blocking=true)
+
+val question = "sum v1 by id1:id2" /** #2 */
+val fun = ".groupBy sum"
+System.gc()
+val ans = X.groupBy("id1","id2").sum("v1")
+val t_start = DateTime.now()
+ans.cache
+val out_rows = ans.count
+val t_end = DateTime.now()
+val t = new Duration(t_start, t_end).getMillis.toDouble / 1000.toDouble
+val m = Double.NaN
+write_log(task=task, data=data_name, in_rows=in_rows, question=question, out_rows=out_rows, solution=solution, version=ver, fun=fun, run=1:Int, time_sec=t, mem_gb=m)
+ans.unpersist(blocking=true)
+System.gc()
+val ans = X.groupBy("id1","id2").sum("v1")
+val t_start = DateTime.now()
+ans.cache
+val out_rows = ans.count
+val t_end = DateTime.now()
+val t = new Duration(t_start, t_end).getMillis.toDouble / 1000.toDouble
+val m = Double.NaN
+write_log(task=task, data=data_name, in_rows=in_rows, question=question, out_rows=out_rows, solution=solution, version=ver, fun=fun, run=2:Int, time_sec=t, mem_gb=m)
+ans.unpersist(blocking=true)
+System.gc()
+val ans = X.groupBy("id1","id2").sum("v1")
+val t_start = DateTime.now()
+ans.cache
+val out_rows = ans.count
+val t_end = DateTime.now()
+val t = new Duration(t_start, t_end).getMillis.toDouble / 1000.toDouble
+val m = Double.NaN
+write_log(task=task, data=data_name, in_rows=in_rows, question=question, out_rows=out_rows, solution=solution, version=ver, fun=fun, run=3:Int, time_sec=t, mem_gb=m)
+ans.unpersist(blocking=true)
+
+val question = "sum v1 mean v3 by id3" /** #3 */
+val fun = ".groupBy agg sum mean"
+System.gc()
+val ans = X.groupBy("id3").agg(sum("v1"), mean("v3"))
+val t_start = DateTime.now()
+ans.cache
+val out_rows = ans.count
+val t_end = DateTime.now()
+val t = new Duration(t_start, t_end).getMillis.toDouble / 1000.toDouble
+val m = Double.NaN
+write_log(task=task, data=data_name, in_rows=in_rows, question=question, out_rows=out_rows, solution=solution, version=ver, fun=fun, run=1:Int, time_sec=t, mem_gb=m)
+ans.unpersist(blocking=true)
+System.gc()
+val ans = X.groupBy("id3").agg(sum("v1"), mean("v3"))
+val t_start = DateTime.now()
+ans.cache
+val out_rows = ans.count
+val t_end = DateTime.now()
+val t = new Duration(t_start, t_end).getMillis.toDouble / 1000.toDouble
+val m = Double.NaN
+write_log(task=task, data=data_name, in_rows=in_rows, question=question, out_rows=out_rows, solution=solution, version=ver, fun=fun, run=2:Int, time_sec=t, mem_gb=m)
+ans.unpersist(blocking=true)
+System.gc()
+val ans = X.groupBy("id3").agg(sum("v1"), mean("v3"))
+val t_start = DateTime.now()
+ans.cache
+val out_rows = ans.count
+val t_end = DateTime.now()
+val t = new Duration(t_start, t_end).getMillis.toDouble / 1000.toDouble
+val m = Double.NaN
+write_log(task=task, data=data_name, in_rows=in_rows, question=question, out_rows=out_rows, solution=solution, version=ver, fun=fun, run=3:Int, time_sec=t, mem_gb=m)
+ans.unpersist(blocking=true)
+
+val question = "mean v1:v3 by id4" /** #4 */
+val fun = ".groupBy mean"
+System.gc()
+val ans = X.groupBy("id4").mean("v1","v2","v3")
+val t_start = DateTime.now()
+ans.cache
+val out_rows = ans.count
+val t_end = DateTime.now()
+val t = new Duration(t_start, t_end).getMillis.toDouble / 1000.toDouble
+val m = Double.NaN
+write_log(task=task, data=data_name, in_rows=in_rows, question=question, out_rows=out_rows, solution=solution, version=ver, fun=fun, run=1:Int, time_sec=t, mem_gb=m)
+ans.unpersist(blocking=true)
+System.gc()
+val ans = X.groupBy("id4").mean("v1","v2","v3")
+val t_start = DateTime.now()
+ans.cache
+val out_rows = ans.count
+val t_end = DateTime.now()
+val t = new Duration(t_start, t_end).getMillis.toDouble / 1000.toDouble
+val m = Double.NaN
+write_log(task=task, data=data_name, in_rows=in_rows, question=question, out_rows=out_rows, solution=solution, version=ver, fun=fun, run=2:Int, time_sec=t, mem_gb=m)
+ans.unpersist(blocking=true)
+System.gc()
+val ans = X.groupBy("id4").mean("v1","v2","v3")
+val t_start = DateTime.now()
+ans.cache
+val out_rows = ans.count
+val t_end = DateTime.now()
+val t = new Duration(t_start, t_end).getMillis.toDouble / 1000.toDouble
+val m = Double.NaN
+write_log(task=task, data=data_name, in_rows=in_rows, question=question, out_rows=out_rows, solution=solution, version=ver, fun=fun, run=3:Int, time_sec=t, mem_gb=m)
+ans.unpersist(blocking=true)
+
+
+val question = "sum v1:v3 by id6" /** #5 */
+val fun = ".groupBy sum"
+System.gc()
+val ans = X.groupBy("id6").sum("v1","v2","v3")
+val t_start = DateTime.now()
+ans.cache
+val out_rows = ans.count
+val t_end = DateTime.now()
+val t = new Duration(t_start, t_end).getMillis.toDouble / 1000.toDouble
+val m = Double.NaN
+write_log(task=task, data=data_name, in_rows=in_rows, question=question, out_rows=out_rows, solution=solution, version=ver, fun=fun, run=1:Int, time_sec=t, mem_gb=m)
+ans.unpersist(blocking=true)
+System.gc()
+val ans = X.groupBy("id6").sum("v1","v2","v3")
+val t_start = DateTime.now()
+ans.cache
+val out_rows = ans.count
+val t_end = DateTime.now()
+val t = new Duration(t_start, t_end).getMillis.toDouble / 1000.toDouble
+val m = Double.NaN
+write_log(task=task, data=data_name, in_rows=in_rows, question=question, out_rows=out_rows, solution=solution, version=ver, fun=fun, run=2:Int, time_sec=t, mem_gb=m)
+ans.unpersist(blocking=true)
+System.gc()
+val ans = X.groupBy("id6").sum("v1","v2","v3")
+val t_start = DateTime.now()
+ans.cache
+val out_rows = ans.count
+val t_end = DateTime.now()
+val t = new Duration(t_start, t_end).getMillis.toDouble / 1000.toDouble
+val m = Double.NaN
+write_log(task=task, data=data_name, in_rows=in_rows, question=question, out_rows=out_rows, solution=solution, version=ver, fun=fun, run=3:Int, time_sec=t, mem_gb=m)
+ans.unpersist(blocking=true)
+
+/** cleanup and exit */
+
+X.unpersist(blocking=true)
+
+sys.exit(0)
--- spark/spark.sh
+++ spark/spark.sh
@@ -5,7 +5,14 @@ set -e
 ./spark/init-spark.sh
 
 # join
-$SPARK_HOME/bin/spark-shell --master $SPARK_MASTER --packages com.databricks:spark-csv_2.10:1.3.0 --num-executors 9 --executor-memory 200g --driver-memory 200g --conf spark.driver.maxResultSize=200g --conf spark.network.timeout=1800 --conf spark.executor.heartbeatInterval=600 -i ./spark/join-spark.scala
+if [[ "$RUN_TASKS" =~ "join" ]]; then
+   $SPARK_HOME/bin/spark-shell --master $SPARK_MASTER --packages com.databricks:spark-csv_2.10:1.3.0 --num-executors 9 --executor-memory 200g --driver-memory 200g --conf spark.driver.maxResultSize=200g --conf spark.network.timeout=1800 --conf spark.executor.heartbeatInterval=600 -i ./spark/join-spark.scala
+fi;
+
+# groupby
+if [[ "$RUN_TASKS" =~ "groupby" ]]; then
+  $SPARK_HOME/bin/spark-shell --master $SPARK_MASTER --packages com.databricks:spark-csv_2.10:1.3.0 --num-executors 9 --executor-memory 200g --driver-memory 200g --conf spark.driver.maxResultSize=200g --conf spark.network.timeout=1800 --conf spark.executor.heartbeatInterval=600 -i ./spark/groupby-spark.scala
+fi;
 
 # shutdown
 ./spark/shutdown-spark.sh
