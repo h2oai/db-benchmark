6fd01e8a9116b346a44194794501380425bf6177
jangorecki <j.gorecki@wit.edu.pl>
2016-07-27 19:13:41 -0700

    upgrade spark to 2.0.0 stable

--- spark/setup-spark.sh
+++ spark/setup-spark.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 set -e
 
-## Requirements:
-# 1. on mr-0xd6 you should have ~/spark-2.0.0-SNAPSHOT-bin-hadoop2.6
+## Spark 2.0
 
-export SPARK_HOME="$HOME/spark-2.0.0-SNAPSHOT-bin-hadoop2.6"
+# https://spark.apache.org/downloads.html
+wget http://d3kbcqa49mib13.cloudfront.net/spark-2.0.0-bin-hadoop2.6.tgz
+md5sum spark-2.0.0-bin-hadoop2.6.tgz
+# 6d9cac116f3434330869dedb6c5fe9ca  spark-2.0.0-bin-hadoop2.6.tgz
+# http://www.apache.org/dist/spark/spark-2.0.0/spark-2.0.0-bin-hadoop2.6.tgz.md5
+tar -xf spark-2.0.0-bin-hadoop2.6.tgz
+
+export SPARK_HOME="$HOME/spark-2.0.0-bin-hadoop2.6"
 export SPARK_WORKER_IP="mr-0xd1 mr-0xd2 mr-0xd3 mr-0xd4 mr-0xd5 mr-0xd7 mr-0xd8 mr-0xd9 mr-0xd10"
 
 # copy spark binaries to nodes
 for i in $SPARK_WORKER_IP; do cmd="rsync -aq $SPARK_HOME $USER@$i:."; echo $cmd; eval $cmd; done
 # local tmp dir
 for i in $SPARK_WORKER_IP; do cmd="ssh $USER@$i 'mkdir -p tmp'"; echo $cmd; eval $cmd; done
+
+
+
+
+
