fd2832de6f8357d746f95d17270a50c64621d6e2
jangorecki <j.gorecki@wit.edu.pl>
2016-08-01 19:52:38 -0700

    h2o upgrade process

--- h2o/setup-h2o.sh
+++ h2o/setup-h2o.sh
@@ -4,5 +4,10 @@ set -e
 ## Requirements:
 # 1. on mr-0xd6 you should have ~/h2o.jar, file originally from h2o-hadoop/h2o-hdp2.2-assembly/build/libs/h2odriver.jar
 
-# copy h2o.jar to nodes
+# build h2o
+BUILD_HADOOP=YeAh ./gradlew :h2o-hadoop:h2o-hdp2.2-assembly:assemble
+mv ./h2o-hadoop/h2o-hdp2.2-assembly/build/libs/h2odriver.jar ~/h2o.jar
+rsync -aq $HOME/h2o.jar $USER@mr-0xd6:h2o.jar
+
+# copy h2o.jar to all nodes
 for i in $CLUSTER; do cmd="rsync -aq $HOME/h2o.jar $USER@$i:h2o.jar"; echo $cmd; eval $cmd; done
