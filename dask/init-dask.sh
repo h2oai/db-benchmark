437f841c6dd01bd53c43043900444c028d02cd01
jangorecki <j.gorecki@wit.edu.pl>
2016-08-05 12:19:43 -0700

    dask proper init and shutdown

--- dask/init-dask.sh
+++ dask/init-dask.sh
@@ -4,7 +4,7 @@ set -e
 [ -z "$CLUSTER" ] && echo "Need to set CLUSTER" && exit 1;
 
 # kcl: always kill all hosts to not leave lying around when changing configs
-killall -9 dask-ssh 2>&1 > /dev/null
+pkill -9 dask-ssh || true
 for i in $CLUSTER; do (ssh $USER@$i "killall -9 python 2>&1 > /dev/null" &) 2>&1 > /dev/null; done && sleep 5
 
 # start cluster
