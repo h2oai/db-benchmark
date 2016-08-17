3401538070219946a6e0ba4e77c5545ac1315e5d
jangorecki <j.gorecki@wit.edu.pl>
2016-08-17 09:44:03 -0700

    presto shutdown works after presto error

--- presto/shutdown-presto.sh
+++ presto/shutdown-presto.sh
@@ -7,7 +7,9 @@ set -e
 echo "Stopping presto cluster..."
 for i in $CLUSTER; do cmd="ssh $USER@$i 'export PRESTO_HOME=$PRESTO_HOME PATH=/usr/lib/jvm/java-8-oracle/bin:\$PATH; $PRESTO_HOME/bin/launcher kill'"; eval $cmd; done;
 sleep 5
+for i in $CLUSTER; do (ssh $USER@$i "killall -9 java 2>&1 > /dev/null" &) 2>&1 > /dev/null; done && sleep 5
+sleep 5
 
 echo "Checking presto cluster stopped correctly: presto-server, java. No pid means ok." # echo \$HOSTNAME; 
-for i in $CLUSTER; do cmd="ssh $USER@$i 'pgrep -u jan presto-server'"; eval $cmd; done;
-for i in $CLUSTER; do cmd="ssh $USER@$i 'pgrep -u jan java'"; eval $cmd; done;
+for i in $CLUSTER; do cmd="ssh $USER@$i 'pgrep -u $USER presto-server'"; eval $cmd; done;
+for i in $CLUSTER; do cmd="ssh $USER@$i 'pgrep -u $USER java'"; eval $cmd; done;
