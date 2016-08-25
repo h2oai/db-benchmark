9bb830342f42fe1b90dd67822b4718299895dfa4
jangorecki <j.gorecki@wit.edu.pl>
2016-08-25 09:33:35 -0700

    spark sort tests

--- spark/spark.sh
+++ spark/spark.sh
@@ -10,7 +10,7 @@ if [[ "$RUN_TASKS" =~ "join" ]]; then
   while read line
   do 
     eval $line
-    $SPARK_HOME/bin/spark-shell --master $SPARK_MASTER --verbose --num-executors 8 --executor-memory 200g --driver-memory 200g --conf spark.driver.maxResultSize=200g --conf spark.network.timeout=2400 --conf spark.executor.heartbeatInterval=1200 -i ./spark/join-spark.scala
+    $SPARK_HOME/bin/spark-shell --master $SPARK_MASTER --verbose --num-executors 9 --executor-memory 200g --driver-memory 200g --conf spark.driver.maxResultSize=200g --conf spark.network.timeout=2400 --conf spark.executor.heartbeatInterval=1200 -i ./spark/join-spark.scala
   done < ./loop-join-data.env
 fi
 
@@ -19,8 +19,17 @@ if [[ "$RUN_TASKS" =~ "groupby" ]]; then
   while read line
   do 
     eval $line
-    $SPARK_HOME/bin/spark-shell --master $SPARK_MASTER --verbose --num-executors 8 --executor-memory 200g --driver-memory 200g --conf spark.driver.maxResultSize=200g --conf spark.network.timeout=2400 --conf spark.executor.heartbeatInterval=1200 -i ./spark/groupby-spark.scala
+    $SPARK_HOME/bin/spark-shell --master $SPARK_MASTER --verbose --num-executors 9 --executor-memory 200g --driver-memory 200g --conf spark.driver.maxResultSize=200g --conf spark.network.timeout=2400 --conf spark.executor.heartbeatInterval=1200 -i ./spark/groupby-spark.scala
   done < ./loop-groupby-data.env
+fi
+
+# sort
+if [[ "$RUN_TASKS" =~ "sort" ]]; then
+  while read line
+  do
+    eval $line
+    $SPARK_HOME/bin/spark-shell --master $SPARK_MASTER --verbose --num-executors 9 --executor-memory 200g --driver-memory 200g --conf spark.driver.maxResultSize=200g --conf spark.network.timeout=2400 --conf spark.executor.heartbeatInterval=1200 -i ./spark/sort-spark.scala
+  done < ./loop-sort-data.env
 fi
 
 # shutdown
