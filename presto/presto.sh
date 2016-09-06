6c8ad6ef4f5a30f38c0ee212afa6afe8e6dd7439
jangorecki <j.gorecki@wit.edu.pl>
2016-09-06 16:40:08 -0700

    presto sort

--- presto/presto.sh
+++ presto/presto.sh
@@ -5,9 +5,9 @@ set -e
 ./presto/init-presto.sh
 sleep 30
 
-rm -f ./presto/join-presto.log ./presto/join-presto-setup-hive.log
+rm -f ./presto/join-presto-setup-hive.log ./presto/join-presto.log #./presto/sort-presto.log
     
-# join
+# join - temporarly also sort
 if [[ "$RUN_TASKS" =~ "join" ]]; then
   echo "Running presto join benchmark..."
   while read line
@@ -21,6 +21,9 @@ if [[ "$RUN_TASKS" =~ "join" ]]; then
     PATH=/usr/lib/jvm/java-8-oracle/bin:$PATH $PRESTO_CLI --catalog hive --schema benchmark --server $MASTER:$PRESTO_PORT --output-format CSV < ./presto/join-presto.sql >> ./presto/join-presto.log 2>&1;
     # parse log, waiting until presto prints the timing
     #./presto/presto-write.log.R "./presto/join-presto.log"
+    #echo "sorting..." # temporary start in join to re-use existing data http://stackoverflow.com/questions/39309984/csv-loading-from-hadoop-to-hive-scalability-issue-20gb-h
+    #PATH=/usr/lib/jvm/java-8-oracle/bin:$PATH $PRESTO_CLI --catalog hive --schema benchmark --server $MASTER:$PRESTO_PORT --output-format CSV < ./presto/sort-presto.sql >> ./presto/sort-presto.log 2>&1;
+    
   done < ./loop-join-data.env
 fi
 cat ./presto/join-presto.log
