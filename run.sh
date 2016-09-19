a7e611808268a127e960fa326abf5b93dc1bffb4
jangorecki <j.gorecki@wit.edu.pl>
2016-09-19 09:09:57 -0700

    check network quality before each distributed solution

--- run.sh
+++ run.sh
@@ -7,10 +7,6 @@ source run.conf
 # ensure all presto shutdown
 ./presto/shutdown-presto.sh || true
 
-# test network speed
-./servertest.sh
-Rscript -e 'data.table::fread("servertest.log")[substr(V3,1,2)!="Gb" | V2 < 9, if (!.N) q("no") else if(any(V2<5)) stop("slow (< 5 Gb) network interface on nodes: ",paste(V1[V2<5],collapse=", "), call.=FALSE) else warning("network interface below 9 Gb on nodes: ",paste(V1,collapse=", "), call.=FALSE)]'
-
 # produce iteration dictionaries from data.csv
 ./init-setup-iteration.R
 
@@ -19,27 +15,42 @@ export BATCH=$(date +%s)
 
 echo "# Benchmark run $BATCH started"
 
+# test network speed
+./servertest.sh
+Rscript -e 'data.table::fread("servertest.log")[substr(V3,1,2)!="Gb" | V2 < 9, if (!.N) q("no") else if(any(V2<5)) stop("slow (< 5 Gb) network interface on nodes: ",paste(V1[V2<5],collapse=", "), call.=FALSE) else warning("network interface below 9 Gb on nodes: ",paste(V1,collapse=", "), call.=FALSE)]'
 # spark
 ./spark/spark.sh
 
+# test network speed
+./servertest.sh
+Rscript -e 'data.table::fread("servertest.log")[substr(V3,1,2)!="Gb" | V2 < 9, if (!.N) q("no") else if(any(V2<5)) stop("slow (< 5 Gb) network interface on nodes: ",paste(V1[V2<5],collapse=", "), call.=FALSE) else warning("network interface below 9 Gb on nodes: ",paste(V1,collapse=", "), call.=FALSE)]'
 # impala
 ./impala/impala.sh
 
 # datatable
 ./datatable/datatable.sh
 
+# test network speed
+./servertest.sh
+Rscript -e 'data.table::fread("servertest.log")[substr(V3,1,2)!="Gb" | V2 < 9, if (!.N) q("no") else if(any(V2<5)) stop("slow (< 5 Gb) network interface on nodes: ",paste(V1[V2<5],collapse=", "), call.=FALSE) else warning("network interface below 9 Gb on nodes: ",paste(V1,collapse=", "), call.=FALSE)]'
 # h2o
 ./h2o/h2o.sh
 
 # pandas
 ./pandas/pandas.sh
 
+# test network speed
+./servertest.sh
+Rscript -e 'data.table::fread("servertest.log")[substr(V3,1,2)!="Gb" | V2 < 9, if (!.N) q("no") else if(any(V2<5)) stop("slow (< 5 Gb) network interface on nodes: ",paste(V1[V2<5],collapse=", "), call.=FALSE) else warning("network interface below 9 Gb on nodes: ",paste(V1,collapse=", "), call.=FALSE)]'
 # dask
 ./dask/dask.sh
 
 # dplyr
 ./dplyr/dplyr.sh
 
+# test network speed
+./servertest.sh
+Rscript -e 'data.table::fread("servertest.log")[substr(V3,1,2)!="Gb" | V2 < 9, if (!.N) q("no") else if(any(V2<5)) stop("slow (< 5 Gb) network interface on nodes: ",paste(V1[V2<5],collapse=", "), call.=FALSE) else warning("network interface below 9 Gb on nodes: ",paste(V1,collapse=", "), call.=FALSE)]'
 # presto
 ./presto/presto.sh
 
@@ -51,4 +62,4 @@ echo "# Benchmark run $BATCH has been completed in $(($(date +%s)-$BATCH))s"
 
 # test network speed
 ./servertest.sh
-Rscript -e 'data.table::fread("servertest.log")[substr(V3,1,2)!="Gb" | V2 < 9.5, if(.N) stop("slow (< 9.5 Gb) network interface on nodes: ",paste(V1,collapse=","), call.=FALSE) else q("no")]'
+Rscript -e 'data.table::fread("servertest.log")[substr(V3,1,2)!="Gb" | V2 < 9.5, if (!.N) q("no") else warning("slow (< 9.5 Gb) network interface on nodes: ",paste(V1,collapse=","), call.=FALSE)]'
