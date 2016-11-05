50143896b1541705d65c861c53febd71b1a75972
jangorecki <j.gorecki@wit.edu.pl>
2016-11-04 17:11:36 -0700

    pandas update cmd, better comment

--- pandas/setup-pandas.sh
+++ pandas/setup-pandas.sh
@@ -6,10 +6,19 @@ set -e
 # install python, pandas and pydoop.hdfs
 apt-get update
 apt-get install build-essential python-dev python-pip
-pip install --upgrade pandas
+sudo pip install --upgrade pandas
 pip install pydoop.hdfs
 
 # check
 python
 import pandas as pd
 pd.__version__
+
+# export ALL_HOSTS="mr-0xd1 mr-0xd2 mr-0xd3 mr-0xd4 mr-0xd5 mr-0xd7 mr-0xd8 mr-0xd9 mr-0xd10"
+# python2 -c 'import pandas as pd; print pd.__version__'
+# python3 -c 'import pandas as pd; print pd.__version__'
+# for i in $ALL_HOSTS; do cmd="ssh $USER@$i 'python2 -c \"import pandas as pd; print pd.__version__\"'"; echo $cmd; eval $cmd; done
+# for i in $ALL_HOSTS; do cmd="ssh $USER@$i 'python3 -c \"import pandas as pd; print pd.__version__\"'"; echo $cmd; eval $cmd; done
+
+# sudo pip install --upgrade pandas
+# sudo pip3 install pandas
--- presto/shutdown-presto.sh
+++ presto/shutdown-presto.sh
@@ -11,6 +11,6 @@ for i in $CLUSTER; do (ssh $USER@$i "killall -9 presto-server 2>&1 > /dev/null" 
 for i in $CLUSTER; do (ssh $USER@$i "killall -9 java 2>&1 > /dev/null" &) 2>&1 > /dev/null; done && sleep 5
 sleep 5
 
-echo "Checking presto cluster stopped correctly: presto-server, java. No pid means ok." # echo \$HOSTNAME; 
+echo "Checking presto cluster stopped correctly: presto-server, java. No pid below means ok." # echo \$HOSTNAME; 
 for i in $CLUSTER; do cmd="ssh $USER@$i 'pgrep -u $USER presto-server'"; eval $cmd; done;
 for i in $CLUSTER; do cmd="ssh $USER@$i 'pgrep -u $USER java'"; eval $cmd; done;
