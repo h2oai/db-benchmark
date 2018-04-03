9c50a10407c3519c23a8d21a8a2e355891022c46
jangorecki <j.gorecki@wit.edu.pl>
2018-04-03 10:13:43 +0530

    re-use update from data.table pkg. Closes #1

--- datatable/helpers-datatable.R
+++ datatable/helpers-datatable.R
@@ -2,6 +2,6 @@ memory_usage = function() {
   as.numeric(system(paste("ps -o rss", Sys.getpid(), "| tail -1"), intern=TRUE)) / (1024^2)
 }
 datatable.git = function() {
-  dcf = read.dcf(system.file("DESCRIPTION", package="data.table"), fields="Commit")
-  toString(dcf[, "Commit"])
+  dcf = read.dcf(system.file("DESCRIPTION", package="data.table"), fields="Revision")
+  toString(dcf[, "Revision"])
 }
