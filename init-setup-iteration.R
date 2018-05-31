efe523b230e456ed70d209ecd944cfab9e9c47af
jangorecki <j.gorecki@wit.edu.pl>
2018-05-31 15:11:08 +0530

    cleanup of iteration setup script

--- init-setup-iteration.R
+++ init-setup-iteration.R
@@ -5,24 +5,8 @@ cat("# init-setup-iteration.R\n")
 run_tasks = Sys.getenv("RUN_TASKS", NA_character_)
 if (is.na(run_tasks)) stop("RUN_TASKS env var not defined.")
 run_tasks = strsplit(run_tasks, " ", fixed=TRUE)[[1L]]
-library(data.table)
-# iters_on_solution_level = function(){
-#   solution_dir = c("spark","impala","datatable","h2o","pandas","dask")
-#   dt = fread("data.csv")
-#   dt = dt[active==TRUE # flag
-#           ][run_tasks, on="task", nomatch=0L # filter for ENV VAR RUN TASKS
-#             ][data.table(active=TRUE, solution_dir=solution_dir), on="active", allow.cartesian=TRUE # cross join solutions
-#               ]
-#   dt[task=="join",
-#      .(iter=sprintf("export %s", paste(paste(c("SRC_X","SRC_Y","SRC_X_LOCAL","SRC_Y_LOCAL"), c(hdfs[1L], hdfs[2L], local[1L], local[2L]), sep="="), collapse=" "))),
-#      .(task, rows, solution_dir)
-#      ][, if(.N) writeLines(iter, con=file.path(solution_dir,"loop-join-data.env"), sep="\n"), .(task, solution_dir)]
-#   dt[task=="groupby",
-#      .(iter=sprintf("export %s", paste(paste(c("SRC_GRP","SRC_GRP_LOCAL"), c(hdfs, local), sep="="), collapse=" "))),
-#      .(task, solution_dir, data)
-#      ][, if(.N) writeLines(iter, con=file.path(solution_dir,"loop-groupby-data.env"), sep="\n"), .(task, solution_dir)]
-#   invisible()
-# }
+
+suppressPackageStartupMessages(library(data.table))
 iters_on_task_level = function(){
   dt = fread("data.csv")
   dt = dt[active==TRUE # flag
