4397eef4a324e768cc0399cf62f136521e570ec4
jangorecki <j.gorecki@wit.edu.pl>
2018-06-02 13:09:11 +0530

    update benchplot for pydatatable

--- benchplot.R
+++ benchplot.R
@@ -1,49 +1,75 @@
+tests = "
+# GROUPING
+DT[, sum(v1), keyby=id1]
+DF %>% group_by(id1) %>% summarise(sum(v1))
+DF.groupby(['id1']).agg({'v1':'sum'})
+DT[:, {'v1': sum(f.v1)}, f.id1]
 
+DT[, sum(v1), keyby=.(id1, id2)]
+DF %>% group_by(id1,id2) %>% summarise(sum(v1))
+DF.groupby(['id1','id2']).agg({'v1':'sum'})
+DT[:, sum(f.v1), [f.id1, f.id2]] 
 
-# Must be in directory where res* files are
+DT[, .(sum(v1), mean(v3)), keyby=id3]
+DF %>% group_by(id3) %>% summarise(sum(v1),mean(v3))
+DF.groupby(['id3']).agg({'v1':'sum', 'v3':'mean'})
+DT[:, {'v1': sum(f.v1), 'v3': mean(f.v3)}, f.id3]
 
-# setwd("~/R/Benchmark_Scripts")
-# OUTPATH = "~/R/gitdatatable/data.table.wiki.git/bench/" # release
+DT[, lapply(.SD, mean), keyby=id4, .SDcols=v1:v3]
+DF %>% group_by(id4) %>% summarise_each(funs(mean), vars=7:9)
+DF.groupby(['id4']).agg({'v1':'mean', 'v2':'mean', 'v3':'mean'})
+DT[:, {'v1': mean(f.v1), 'v2': mean(f.v2), 'v3': mean(f.v3)}, f.id4]
 
-benchplot = function(.nrow=1e7) {
-  require(data.table)
-  if (!exists("OUTPATH")) OUTPATH = paste0(getwd(),"/")  # dev
-  fnam = paste0(OUTPATH,"grouping.",gsub("e[+]0","E",.nrow),".png")
+DT[, lapply(.SD, sum), keyby=id6, .SDcols=v1:v3]
+DF %>% group_by(id6) %>% summarise_each(funs(sum), vars=7:9)
+DF.groupby(['id6']).agg({'v1':'sum', 'v2':'sum', 'v3':'sum'})
+DT[:, {'v1': sum(f.v1), 'v2': sum(f.v2), 'v3': sum(f.v3)}, f.id6]
+"
+
+library(data.table)
+benchplot = function(.nrow=1e6) {
+  fnam = paste0("grouping.",gsub("e[+]0","E",.nrow),".png")
   cat("Plotting to",fnam,"...\n")
   png(file = fnam, width=800, height=1000)
   
   par(mar=c(1.1,1.1,6.1,2.1)) # shift to the left
-  
-  res = rbindlist( lapply(dir(patt="*.csv"), fread) )
-  
-  if (res[, any(abs(((user+sys) / elapsed)-1)>0.01 & user>0.0) ]) stop("user+sys != elapsed +/- 1%")
-  # Python's timeit doesn't give us user and sys separately it seems
-  
-  res[,c("user","sys","user.child","sys.child"):=NULL]
-  res = res[test>0]
 
-  # sometimes the first row is read others not when there is no header.
-  # *** TO DO: fread bug, TO DO
+  res = fread("time.csv")[batch==max(batch)][task=="groupby"][, task:=NULL]
+  res=res[, .SD, .SDcols=c("time_sec","question","solution","in_rows","out_rows","out_cols","run","version","git")]
+  res[, test := setNames(1:5, unique(res$question))[question]]
+  setnames(res, c("time_sec","question","solution","in_rows","out_rows","out_cols"), c("elapsed","task","pkg","nrow","ansnrow","ansncol"))
+  gb = fread("data.csv")[rows==.nrow & task=="groupby" & active==TRUE, gb]
+  stopifnot(length(gb)==1L)
+  res[, gb:=gb]
 
-  ans = res[nrow==.nrow & run==1][base::order(test,pkg,decreasing=TRUE)]
-                     # decreasing=TRUE for when we work with v1.9.2 on CRAN
+  # datatable#1082 grouping test2 currently (0,0) dummy frame
+  res[pkg=="pydatatable" & task=="sum v1 by id1:id2", elapsed:=NA_real_]
+  fix_missing = res[pkg=="data.table" & task=="sum v1 by id1:id2", .(ansnrow, ansncol)]
+  res[pkg=="pydatatable" & task=="sum v1 by id1:id2", c("ansnrow","ansncol") := fix_missing]
   
-  NPKG = 3L # data.table, dplyr and pandas
+  ans = res[nrow==.nrow & run==1][order(test,pkg,decreasing=TRUE)]
+  
+  NPKG = 4L # data.table, dplyr, pandas, pydatatable
   pad = as.vector(sapply(0:4, function(x) c( as.vector(rbind(x*NPKG + 1:NPKG, NA)), NA, NA)))
 
-  # tests variable from other file source first
   code = unlist(strsplit(tests, split="\n"))[-(1:2)]
   code = code[code!=""]
-
+  
   # horiz=TRUE does it first bar from the bottom
   comma = function(x) format(as.integer(signif(x,4)),big.mark=",")
   lr = "#FF7777"
   lb = "#7777FF"
   lg = "#77FF77"; green = "green4"
+  pydtcol = "orange"
+  lpydtcol = "orange2"
   m = ans[,max(elapsed,na.rm=TRUE)]
-  if (m>2*60*60) { timescale=3600; xlab="Hours" }
-  else if (m>120) { timescale=60; xlab="Minutes" }
-  else { timescale=1; xlab="Seconds" }
+  if (m>2*60*60) {
+    timescale=3600; xlab="Hours"
+  } else if (m>120) {
+    timescale=60; xlab="Minutes"
+  } else {
+    timescale=1; xlab="Seconds"
+  }
   bars = ans[,(elapsed)/timescale]
   at = pretty(bars,10)
   at = at[at!=0]
@@ -61,13 +87,12 @@ benchplot = function(.nrow=1e7) {
   abline(h=h2)
   text(x=at[ff], y=h2, labels=format(at[ff]), adj=c(0.5,+1.5), cex=1.5, font=2, xpd=NA)
   text(x=0, y=h2, labels=xlab, adj=c(0,+1.5), font=2, cex=1.5, xpd=NA)
-  # mgp= (see par) brings tick labels closer to axis, but we don't use axis() here.
   
   space = NPKG*2 + 2
   abline(h=tt[seq(space+1,by=space,length=4)], col="grey",lwd=2)
   for (x in at) lines(x=c(x,x), y=c(h1,h2), col="lightgrey", lwd=2, lty="dotted")
-  barplot(bars[pad], horiz=TRUE, axes=FALSE, # xlim=c(0,tail(at,1)),
-          col=rep(c(green,"red","blue","black"),each=2), font=2, xpd=NA, add=TRUE)
+  barplot(bars[pad], horiz=TRUE, axes=FALSE,
+          col=rep(c(pydtcol,green,"red","blue","black"),each=2), font=2, xpd=NA, add=TRUE)
   
   textBG = function(x,y,txt,...) {
     txtw = strwidth(txt, ...); txth = strheight(txt, ...);
@@ -80,10 +105,11 @@ benchplot = function(.nrow=1e7) {
     textBG(0, tt[3+i*space], code[1+i*NPKG], col="blue", font=2)
     textBG(0, tt[5+i*space], code[2+i*NPKG], col="red", font=2)
     textBG(0, tt[7+i*space], code[3+i*NPKG], col=green, font=2)
+    textBG(0, tt[9+i*space], code[4+i*NPKG], col=pydtcol, font=2)
     ansnrow = ans[test==i+1 & run==1,ansnrow]
     ansncol = ans[test==i+1 & run==1,ansncol]
     if (length(unique(ansnrow)) != 1) stop("ansnrow mismatch")
-    if (length(unique(ansncol)) != 1) stop("ansncol mismatch")
+    #if (length(unique(ansncol)) != 1) stop("ansncol mismatch") # pd.ans.shape[1] does not return the actual columns and ans is pivot like
     ansnrow = ansnrow[1]
     ansncol = ansncol[1]
     textBG(0, tt[2+i*space], font=2, paste("Test", i+1, ":",
@@ -96,29 +122,30 @@ benchplot = function(.nrow=1e7) {
     at=tt[4+i*space]; rect(0, at-w, ans2[1+i*NPKG, (elapsed)/timescale], at+w, col=lb, xpd=NA)
     at=tt[6+i*space]; rect(0, at-w, ans2[2+i*NPKG, (elapsed)/timescale], at+w, col=lr, xpd=NA)
     at=tt[8+i*space]; rect(0, at-w, ans2[3+i*NPKG, (elapsed)/timescale], at+w, col=lg, xpd=NA)
+    at=tt[10+i*space]; rect(0, at-w, ans2[4+i*NPKG, (elapsed)/timescale], at+w, col=lpydtcol, xpd=NA)
     if (is.na(ans2[3+i*NPKG, elapsed])) textBG(0, tt[8+i*space], "MemoryError", col=green, font=2)
+    if (is.na(ans2[4+i*NPKG, elapsed])) textBG(0, tt[10+i*space], "Not yet implemented", col=pydtcol, font=2)
   }
   t1 = res[nrow==.nrow & pkg=="data.table", sum(elapsed)]
   t2 = res[nrow==.nrow & pkg=="dplyr", sum(elapsed)]
   t3 = res[nrow==.nrow & pkg=="pandas", sum(elapsed)]
+  t4 = res[nrow==.nrow & pkg=="pydatatable", sum(elapsed, na.rm=TRUE)] # TODO rm na.rm after test2 ready datatable#1082
   cph = 0.30  # minimum on graph histories; what people will see if they check
-  legend(0,par()$usr[4]+14*w, pch=22, pt.bg=c("blue","red",green), bty="n", cex=1.5, pt.cex=3.5,
+  leg = unique(res, by="pkg"
+               )[, tN:=c(t1,t2,t3,t4)
+                 ][, hasgit:=!is.na(git) && nzchar(git), by=pkg
+                   ][, sprintf("%s %s %s  -  date  -  Total: $%.02f for %s %s", pkg, version, if (hasgit) paste0("(",substr(git,1,7),")")  else "", cph*tN/3600, round(tN/timescale,0), tolower(xlab)), by=pkg
+                     ][, V1]
+  topoffset = 18.5
+  legend(0,par()$usr[4]+topoffset*w, pch=22, pt.bg=c("blue","red",green,pydtcol), bty="n", cex=1.5, pt.cex=3.5,
          text.font=1, xpd=NA,
-         legend=c(paste0("data.table 1.9.2  -  CRAN 27 Feb 2014  -  Total: $",
-                    sprintf("%.02f",cph*t1/3600)," for ",round(t1/timescale,0)," ",tolower(xlab)),
-                  paste0("dplyr 0.2  -  CRAN 21 May 2014  -  Total: $",
-                    sprintf("%.02f",cph*t2/3600)," for ",round(t2/timescale,0)," ",tolower(xlab)),
-                  paste0("pandas 0.14.1  -  PyPI 11 Jul 2014  -  Total: $",
-                    sprintf("%.02f",cph*t3/3600)," for ",round(t3/timescale,0)," ",tolower(xlab))))
+         legend=leg)
   mtext(paste("Input table:",comma(.nrow),"rows x 9 columns (",
         {gb<-ans[pkg=="data.table",gb[1]]; if (gb<1) round(gb,1) else 5*round(ceiling(gb)/5)},"GB ) - Random order"),
         side=3, line=4.5, cex=1.5, adj=0, font=2)
-  # note that pandas uses double the memory, but we use the smaller data.table size in the title
 
-  legend(par()$usr[2], par()$usr[4]+14*w, pch=22, xpd=NA, xjust=1, bty="n", pt.lwd=1,  #adj=1, # , # , #
+  legend(par()$usr[2], par()$usr[4]+topoffset*w, pch=22, xpd=NA, xjust=1, bty="n", pt.lwd=1,
          legend=c("First time","Second time"), pt.cex=c(3.5,2.5), cex=1.5, pt.bg=c("blue",lb))
   dev.off()
-  system(paste("/usr/bin/eom",fnam), wait=FALSE) 
+  system(paste("/usr/bin/xdg-open",fnam), wait=FALSE) 
 }
-
-
