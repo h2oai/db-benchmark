6769834ebea8bcb54843d5260bd53dc89f1a85c7
jangorecki <j.gorecki@wit.edu.pl>
2018-06-14 12:22:35 +0200

    placeholder for pydataable upgrade only on new git hash

--- pydatatable/init-pydatatable.sh
+++ pydatatable/init-pydatatable.sh
@@ -6,6 +6,9 @@ echo 'upgrading pydatatable...'
 # at this point python virtual env should be sourced and LLVM6 env var set
 
 rm -rf ./tmp/datatable
+
+# python 'import datatable as dt; if (system("git rev-parse HEAD") == dt.__git_revision__) exit()'
+
 mkdir -p ./tmp/datatable
 
 git clone --depth=1 https://github.com/h2oai/datatable.git ./tmp/datatable &> /dev/null
