19f64e42d7201f3d84d201e7cc6ebf9a36ea386c
jangorecki <j.gorecki@wit.edu.pl>
2018-06-16 19:40:26 +0200

    cleanup tree from multinode tools

--- dask/shutdown-dask.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-[ -z "$CLUSTER" ] && echo "Need to set CLUSTER" && exit 1;
-
-echo "Stopping dask cluster..."
-(pkill -9 dask-scheduler && pkill -9 bokeh) || true
-for i in $SLAVES; do (ssh $USER@$i "killall -9 dask-worker 2>&1 > /dev/null" &) 2>&1 > /dev/null; done && sleep 5
-for i in $CLUSTER; do (ssh $USER@$i "killall -9 python 2>&1 > /dev/null" &) 2>&1 > /dev/null; done && sleep 5
