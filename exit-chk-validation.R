faa78728265546cda458e0d6023c326b5650de81
jangorecki <j.gorecki@wit.edu.pl>
2018-09-24 11:10:02 +0530

    use latest batch in validation

--- exit-chk-validation.R
+++ exit-chk-validation.R
@@ -7,6 +7,7 @@ source("helpers.R")
 DT = read_timing("time.csv")
 # recent timings, single cache=FALSE scenario where available
 dt = last_timing(x=DT)
+dt=dt[batch==max(batch, na.rm=TRUE)]
 
 # uniqueness HAVING - detect lack of consistency in query output within single benchmark runs - only recent benchmark run
 uniqueness = function(dt, const=c("out_rows","chk","batch"), const.by=c("task", "data", "in_rows", "question", "solution")) {
@@ -39,7 +40,7 @@ chk.approx = function(dt, precision=4) {
             ][, c(vcols) := lapply(.SD, type.convert), .SDcols=vcols
               ][, paste0("mean_",vcols) := lapply(.SD, mean), .SDcols=vcols
                 ][, paste0("rel_",vcols) := eval(as.call(c(as.name("list"), lapply(vcols, function(col) substitute(round(abs(x-mean_x)/mean_x, precision), list(x=as.name(col), mean_x=as.name(paste0("mean_", col))))))))
-                  ][, .(mean_rel_chk = mean(unlist(.SD))), c("task","data","in_rows","question","solution","chk"), .SDcols=paste0("rel_",vcols)
+                  ][, .(mean_rel_chk = format(mean(unlist(.SD)), scientific = FALSE)), c("task","data","in_rows","question","solution","chk"), .SDcols=paste0("rel_",vcols)
                     ]
   }
   rbindlist(lapply(ldt, diff.chk))
@@ -62,3 +63,4 @@ if (nrow(report <- rbindlist(c(
   list(count = count.check),
   list(chk = chk.check) # not yet approximate
 ), use.names = TRUE, fill = TRUE, idcol = "check"))) submit(report)
+
