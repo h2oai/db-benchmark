e958aa00834cbd7079cd2a0473a0ea135c48ff54
jangorecki <j.gorecki@wit.edu.pl>
2018-10-03 16:01:02 +0530

    julia completed groupby

--- helpers.jl
+++ helpers.jl
@@ -1,21 +1,37 @@
+using Printf; # sprintf macro to print in non-scientific format
+
 function memory_usage()
-  missing
+  pid = getpid()
+  s = read(pipeline(`ps -o rss $pid`,`tail -1`), String)
+  parse(Float64, replace(s, "\n" => "")) / (1024^2)
 end;
 
 function make_chk(x)
-  string(x)
+  n = length(x)
+  res = ""
+  for i = 1:n
+    res = string(res, i==1 ? "" : ";", @sprintf("%0.3f", x[i]))
+  end
+  res
 end;
 
 function write_log(run, task, data, in_rows, question, out_rows, out_cols, solution, version, git, fun, time_sec, mem_gb, cache, chk, chk_time_sec)
-  file="time-julia.csv"
-  #batch=ENV["BATCH"]
-  batch="" # TODO default
+  file==try
+    ENV["CSV_TIME_FILE"]
+  catch
+    "time.csv"
+  end;
+  batch=try
+    ENV["BATCH"]
+  catch
+    ""
+  end;
   comment="" # placeholder for updates to timing data
   time_sec=round(time_sec, digits=3)
-  mem_gb="" #round(mem_gb, digits=3)
+  mem_gb=round(mem_gb, digits=3)
   chk_time_sec=round(chk_time_sec, digits=3)
-  timestamp=time()
-  csv_verbose = false # TODO default and print: ENV["CSV_VERBOSE"]
-  log = DataFrame(batch=batch, timestamp=timestamp, task=task, data=data, in_rows=in_rows, question=question, out_rows=out_rows, out_cols=out_cols, solution=solution, version=version, git=git, fun=fun, run=run, time_sec=time_sec, mem_gb=mem_gb, cache=cache, chk=chk, chk_time_sec=chk_time_sec, comment=comment)
+  timestamp=@sprintf("%0.6f", time())
+  csv_verbose = false # hardcoded for now, TODO ENV["CSV_VERBOSE"] and print
+  log = DataFrame(batch=batch, timestamp=timestamp, task=task, data=data, in_rows=in_rows, question=question, out_rows=out_rows, out_cols=out_cols, solution=solution, version=version, git=git, fun=fun, run=run, time_sec=time_sec, mem_gb=mem_gb, cache=uppercase(string(cache)), chk=chk, chk_time_sec=chk_time_sec, comment=comment)
   CSV.write(file, log, append=true)
 end;
--- julia/groupby-julia.jl
+++ julia/groupby-julia.jl
@@ -1,17 +1,19 @@
 #!/usr/bin/env julia
 
-print("# groupby-julia\n");
+print("# groupby-juliadf\n");
 
 using DataFrames;
 using CSV;
+using Statistics; # mean function
+Using Pkg; # to get DataFrames version
 
 include("./../helpers.jl");
 
-ver = "";
+ver = string(Pkg.installed()["DataFrames"]);
 git = "";
 task = "groupby";
-solution = "julia";
-fun = "";
+solution = "juliadf";
+fun = "by";
 cache = true;
 
 print("loading dataset...\n");
@@ -24,30 +26,186 @@ println(in_rows);
 
 print("grouping...\n");
 
+#ANS = aggregate(x[[:id1, :v1]], :id1, sum);
+# above call:
+#   cannot run sum(v1), mean(v2)
+#   does not retain names
+# thus we use `by ... DataFrame`, if it is possible to improve that please report
+# see #30 and xiaodaigh/FastGroupBy.jl#7
+
 question = "sum v1 by id1"; #1
 GC.gc();
 t_start = time_ns();
-ANS = aggregate(x[[:id1, :v1]], :id1, sum);
+ANS = by(x, [:id1, :id2]) do df; DataFrame(v1 = sum(df.v1)); end;
 println(size(ANS));
 t = (time_ns() - t_start)/1.0e9;
 m = memory_usage();
 t_start = time_ns();
-chk = sum(ANS.v1_sum);
+chk = sum(ANS.v1);
 chkt = (time_ns() - t_start)/1.0e9;
 write_log(1, task, data_name, in_rows, question, size(ANS, 1), size(ANS, 2), solution, ver, git, fun, t, m, cache, make_chk(chk), chkt);
 ANS = 0;
+GC.gc();
+t_start = time_ns();
+ANS = by(x, [:id1, :id2]) do df; DataFrame(v1 = sum(df.v1)); end;
+println(size(ANS));
+t = (time_ns() - t_start)/1.0e9;
+m = memory_usage();
+t_start = time_ns();
+chk = sum(ANS.v1);
+chkt = (time_ns() - t_start)/1.0e9;
+write_log(2, task, data_name, in_rows, question, size(ANS, 1), size(ANS, 2), solution, ver, git, fun, t, m, cache, make_chk(chk), chkt);
+ANS = 0;
+GC.gc();
+t_start = time_ns();
+ANS = by(x, [:id1, :id2]) do df; DataFrame(v1 = sum(df.v1)); end;
+println(size(ANS));
+t = (time_ns() - t_start)/1.0e9;
+m = memory_usage();
+t_start = time_ns();
+chk = sum(ANS.v1);
+chkt = (time_ns() - t_start)/1.0e9;
+write_log(3, task, data_name, in_rows, question, size(ANS, 1), size(ANS, 2), solution, ver, git, fun, t, m, cache, make_chk(chk), chkt);
+ANS = 0;
 
-# 2
-#fn, byveccv, val = +, (x[:id1], x[:id2]), x[:v1]
-#fgroupreduce(+, (x[:id1], x[:id2]), x[:v1])
+question = "sum v1 by id1:id2" #2
+GC.gc();
+t_start = time_ns();
+ANS = by(x, [:id1, :id2]) do df; DataFrame(v1 = sum(df.v1)); end;
+println(size(ANS));
+t = (time_ns() - t_start)/1.0e9;
+m = memory_usage();
+t_start = time_ns();
+chk = sum(ANS.v1);
+chkt = (time_ns() - t_start)/1.0e9;
+write_log(1, task, data_name, in_rows, question, size(ANS, 1), size(ANS, 2), solution, ver, git, fun, t, m, cache, make_chk(chk), chkt);
+ANS = 0;
+GC.gc();
+t_start = time_ns();
+ANS = by(x, [:id1, :id2]) do df; DataFrame(v1 = sum(df.v1)); end;
+println(size(ANS));
+t = (time_ns() - t_start)/1.0e9;
+m = memory_usage();
+t_start = time_ns();
+chk = sum(ANS.v1);
+chkt = (time_ns() - t_start)/1.0e9;
+write_log(2, task, data_name, in_rows, question, size(ANS, 1), size(ANS, 2), solution, ver, git, fun, t, m, cache, make_chk(chk), chkt);
+ANS = 0;
+GC.gc();
+t_start = time_ns();
+ANS = by(x, [:id1, :id2]) do df; DataFrame(v1 = sum(df.v1)); end;
+println(size(ANS));
+t = (time_ns() - t_start)/1.0e9;
+m = memory_usage();
+t_start = time_ns();
+chk = sum(ANS.v1);
+chkt = (time_ns() - t_start)/1.0e9;
+write_log(3, task, data_name, in_rows, question, size(ANS, 1), size(ANS, 2), solution, ver, git, fun, t, m, cache, make_chk(chk), chkt);
+ANS = 0;
 
-# 3
-#fastby((sum, mean), x[:id3], (x[:v1], x[:v3]))
+question = "sum v1 mean v3 by id3" #3
+GC.gc();
+t_start = time_ns();
+ANS = by(x, :id3) do df; DataFrame(v1 = sum(df.v1), v3 = mean(df.v3)); end;
+println(size(ANS));
+t = (time_ns() - t_start)/1.0e9;
+m = memory_usage();
+t_start = time_ns();
+chk = [sum(ANS.v1), sum(ANS.v3)];
+chkt = (time_ns() - t_start)/1.0e9;
+write_log(1, task, data_name, in_rows, question, size(ANS, 1), size(ANS, 2), solution, ver, git, fun, t, m, cache, make_chk(chk), chkt);
+ANS = 0;
+GC.gc();
+t_start = time_ns();
+ANS = by(x, :id3) do df; DataFrame(v1 = sum(df.v1), v3 = mean(df.v3)); end;
+println(size(ANS));
+t = (time_ns() - t_start)/1.0e9;
+m = memory_usage();
+t_start = time_ns();
+chk = [sum(ANS.v1), sum(ANS.v3)];
+chkt = (time_ns() - t_start)/1.0e9;
+write_log(2, task, data_name, in_rows, question, size(ANS, 1), size(ANS, 2), solution, ver, git, fun, t, m, cache, make_chk(chk), chkt);
+ANS = 0;
+GC.gc();
+t_start = time_ns();
+ANS = by(x, :id3) do df; DataFrame(v1 = sum(df.v1), v3 = mean(df.v3)); end;
+println(size(ANS));
+t = (time_ns() - t_start)/1.0e9;
+m = memory_usage();
+t_start = time_ns();
+chk = [sum(ANS.v1), sum(ANS.v3)];
+chkt = (time_ns() - t_start)/1.0e9;
+write_log(3, task, data_name, in_rows, question, size(ANS, 1), size(ANS, 2), solution, ver, git, fun, t, m, cache, make_chk(chk), chkt);
+ANS = 0;
 
-# 4
-#fastby((mean, mean, mean), x[:id4], (x[:v1], x[:v2], x[:v3]));
+question = "mean v1:v3 by id4" #4
+GC.gc();
+t_start = time_ns();
+ANS = by(x, :id4) do df; DataFrame(v1 = mean(df.v1), v2 = mean(df.v2), v3 = mean(df.v3)); end;
+println(size(ANS));
+t = (time_ns() - t_start)/1.0e9;
+m = memory_usage();
+t_start = time_ns();
+chk = [sum(ANS.v1), sum(ANS.v2), sum(ANS.v3)];
+chkt = (time_ns() - t_start)/1.0e9;
+write_log(1, task, data_name, in_rows, question, size(ANS, 1), size(ANS, 2), solution, ver, git, fun, t, m, cache, make_chk(chk), chkt);
+ANS = 0;
+GC.gc();
+t_start = time_ns();
+ANS = by(x, :id4) do df; DataFrame(v1 = mean(df.v1), v2 = mean(df.v2), v3 = mean(df.v3)); end;
+println(size(ANS));
+t = (time_ns() - t_start)/1.0e9;
+m = memory_usage();
+t_start = time_ns();
+chk = [sum(ANS.v1), sum(ANS.v2), sum(ANS.v3)];
+chkt = (time_ns() - t_start)/1.0e9;
+write_log(2, task, data_name, in_rows, question, size(ANS, 1), size(ANS, 2), solution, ver, git, fun, t, m, cache, make_chk(chk), chkt);
+ANS = 0;
+GC.gc();
+t_start = time_ns();
+ANS = by(x, :id4) do df; DataFrame(v1 = mean(df.v1), v2 = mean(df.v2), v3 = mean(df.v3)); end;
+println(size(ANS));
+t = (time_ns() - t_start)/1.0e9;
+m = memory_usage();
+t_start = time_ns();
+chk = [sum(ANS.v1), sum(ANS.v2), sum(ANS.v3)];
+chkt = (time_ns() - t_start)/1.0e9;
+write_log(3, task, data_name, in_rows, question, size(ANS, 1), size(ANS, 2), solution, ver, git, fun, t, m, cache, make_chk(chk), chkt);
+ANS = 0;
 
-# 5
-#fastby((sum, sum, sum), x[:id6], (x[:v1], x[:v2], x[:v3]));
+question = "sum v1:v3 by id6" #5
+GC.gc();
+t_start = time_ns();
+ANS = by(x, :id6) do df; DataFrame(v1 = sum(df.v1), v2 = sum(df.v2), v3 = sum(df.v3)); end;
+println(size(ANS));
+t = (time_ns() - t_start)/1.0e9;
+m = memory_usage();
+t_start = time_ns();
+chk = [sum(ANS.v1), sum(ANS.v2), sum(ANS.v3)];
+chkt = (time_ns() - t_start)/1.0e9;
+write_log(1, task, data_name, in_rows, question, size(ANS, 1), size(ANS, 2), solution, ver, git, fun, t, m, cache, make_chk(chk), chkt);
+ANS = 0;
+GC.gc();
+t_start = time_ns();
+ANS = by(x, :id6) do df; DataFrame(v1 = sum(df.v1), v2 = sum(df.v2), v3 = sum(df.v3)); end;
+println(size(ANS));
+t = (time_ns() - t_start)/1.0e9;
+m = memory_usage();
+t_start = time_ns();
+chk = [sum(ANS.v1), sum(ANS.v2), sum(ANS.v3)];
+chkt = (time_ns() - t_start)/1.0e9;
+write_log(2, task, data_name, in_rows, question, size(ANS, 1), size(ANS, 2), solution, ver, git, fun, t, m, cache, make_chk(chk), chkt);
+ANS = 0;
+GC.gc();
+t_start = time_ns();
+ANS = by(x, :id6) do df; DataFrame(v1 = sum(df.v1), v2 = sum(df.v2), v3 = sum(df.v3)); end;
+println(size(ANS));
+t = (time_ns() - t_start)/1.0e9;
+m = memory_usage();
+t_start = time_ns();
+chk = [sum(ANS.v1), sum(ANS.v2), sum(ANS.v3)];
+chkt = (time_ns() - t_start)/1.0e9;
+write_log(3, task, data_name, in_rows, question, size(ANS, 1), size(ANS, 2), solution, ver, git, fun, t, m, cache, make_chk(chk), chkt);
+ANS = 0;
 
 exit();
