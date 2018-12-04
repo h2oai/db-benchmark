365e091cc9f2a8ea775d4b1985a2fe80037eb32d
jangorecki <j.gorecki@wit.edu.pl>
2018-12-04 22:18:41 +0530

    julia simpler syntax, update benchplot syntax labels also

--- benchplot.R
+++ benchplot.R
@@ -7,7 +7,7 @@ groupby.code = list(
     "dask"="x.groupby(['id1']).agg({'v1':'sum'}).compute()",
     "data.table"="DT[, .(v1=sum(v1)), by=id1]",
     "dplyr"="DF %>% group_by(id1) %>% summarise(sum(v1))",
-    "juliadf"="by(x, :id1) do df; DataFrame(v1 = sum(df.v1)); end;",
+    "juliadf"="by(x, :id1, v1 = :v1=>sum)",
     "pandas"="DF.groupby(['id1']).agg({'v1':'sum'})",
     "pydatatable"="DT[:, {'v1': sum(f.v1)}, by(f.id1)]",
     "spark"="spark.sql('select sum(v1) as v1 from x group by id1')"
@@ -16,7 +16,7 @@ groupby.code = list(
     "dask"="x.groupby(['id1','id2']).agg({'v1':'sum'}).compute()",
     "data.table"="DT[, .(v1=sum(v1)), by=.(id1, id2)]",
     "dplyr"="DF %>% group_by(id1,id2) %>% summarise(sum(v1))",
-    "juliadf"="by(x, [:id1, :id2]) do df; DataFrame(v1 = sum(df.v1)); end;",
+    "juliadf"="by(x, [:id1, :id2], v1 = :v1=>sum)",
     "pandas"="DF.groupby(['id1','id2']).agg({'v1':'sum'})",
     "pydatatable"="DT[:, {'v1': sum(f.v1)}, by(f.id1, f.id2)]",
     "spark"="spark.sql('select sum(v1) as v1 from x group by id1, id2')"
@@ -25,7 +25,7 @@ groupby.code = list(
     "dask"="x.groupby(['id3']).agg({'v1':'sum', 'v3':'mean'}).compute()",
     "data.table"="DT[, .(v1=sum(v1), v3=mean(v3)), by=id3]",
     "dplyr"="DF %>% group_by(id3) %>% summarise(sum(v1), mean(v3))",
-    "juliadf"="by(x, :id3) do df; DataFrame(v1 = sum(df.v1), v3 = mean(df.v3)); end;",
+    "juliadf"="by(x, :id3, v1 = :v1=>sum, v3 = :v3=>mean)",
     "pandas"="DF.groupby(['id3']).agg({'v1':'sum', 'v3':'mean'})",
     "pydatatable"="DT[:, {'v1': sum(f.v1), 'v3': mean(f.v3)}, by(f.id3)]",
     "spark"="spark.sql('select sum(v1) as v1, mean(v3) as v3 from x group by id3')"
@@ -34,7 +34,7 @@ groupby.code = list(
     "dask"="x.groupby(['id4']).agg({'v1':'mean', 'v2':'mean', 'v3':'mean'}).compute()",
     "data.table"="DT[, lapply(.SD, mean), by=id4, .SDcols=v1:v3]",
     "dplyr"="DF %>% group_by(id4) %>% summarise_each(funs(mean), vars=7:9)",
-    "juliadf"="by(x, :id4) do df; DataFrame(v1 = mean(df.v1), v2 = mean(df.v2), v3 = mean(df.v3)); end;",
+    "juliadf"="by(x, :id4, v1 = :v1=>mean, v2 = :v2=>mean, v3 = :v3=>mean)",
     "pandas"="DF.groupby(['id4']).agg({'v1':'mean', 'v2':'mean', 'v3':'mean'})",
     "pydatatable"="DT[:, {'v1': mean(f.v1), 'v2': mean(f.v2), 'v3': mean(f.v3)}, by(f.id4)]",
     "spark"="spark.sql('select mean(v1) as v1, mean(v2) as v2, mean(v3) as v3 from x group by id4')"
@@ -43,7 +43,7 @@ groupby.code = list(
     "dask"="x.groupby(['id6']).agg({'v1':'sum', 'v2':'sum', 'v3':'sum'}).compute()",
     "data.table"="DT[, lapply(.SD, sum), by=id6, .SDcols=v1:v3]",
     "dplyr"="DF %>% group_by(id6) %>% summarise_each(funs(sum), vars=7:9)",
-    "juliadf"="by(x, :id6) do df; DataFrame(v1 = sum(df.v1), v2 = sum(df.v2), v3 = sum(df.v3)); end;",
+    "juliadf"="by(x, :id6, v1 = :v1=>sum, v2 = :v2=>sum, v3 = :v3=>sum)",
     "pandas"="DF.groupby(['id6']).agg({'v1':'sum', 'v2':'sum', 'v3':'sum'})",
     "pydatatable"="DT[:, {'v1': sum(f.v1), 'v2': sum(f.v2), 'v3': sum(f.v3)}, by(f.id6)]",
     "spark"="spark.sql('select sum(v1) as v1, sum(v2) as v2, sum(v3) as v3 from x group by id6')"
