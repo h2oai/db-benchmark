053a1cf9d1472080cb14d941f17915492c399c4b
jangorecki <j.gorecki@wit.edu.pl>
2018-12-12 18:38:13 +0530

    pandas setup also has to install pydt

--- pandas/setup-pandas.sh
+++ pandas/setup-pandas.sh
@@ -11,6 +11,9 @@ source pandas/py-pandas/bin/activate
 # install binaries
 python -m pip install --upgrade pandas
 
+# install datatable for fast data import from jay format
+pip install https://s3.amazonaws.com/h2o-release/datatable/stable/datatable-0.7.0/datatable-0.7.0-cp36-cp36m-linux_x86_64.whl
+
 # check
 python
 import pandas as pd
