388bd8bf56ca32635b1d532227fc5d7f907aa1c1
jangorecki <j.gorecki@wit.edu.pl>
2019-01-10 16:55:54 +0530

    julia q6-q9 and simplify for using elapsed macro

--- juliadf/groupby-juliadf.jl
+++ juliadf/groupby-juliadf.jl
@@ -28,25 +28,15 @@ print("grouping...\n"); flush(stdout);
 
 question = "sum v1 by id1"; # q1
 GC.gc();
-t_start = time_ns();
-ANS = by(x, :id1, v1 = :v1=>sum);
-println(size(ANS)); flush(stdout);
-t = (time_ns() - t_start)/1.0e9;
+t = @elapsed (ANS = by(x, :id1, v1 = :v1=>sum); println(size(ANS)); flush(stdout));
 m = memory_usage();
-t_start = time_ns();
-chk = sum(ANS.v1);
-chkt = (time_ns() - t_start)/1.0e9;
+chkt = @elapsed chk = sum(ANS.v1);
 write_log(1, task, data_name, in_rows, question, size(ANS, 1), size(ANS, 2), solution, ver, git, fun, t, m, cache, make_chk(chk), chkt);
 ANS = 0;
 GC.gc();
-t_start = time_ns();
-ANS = by(x, :id1, v1 = :v1=>sum);
-println(size(ANS)); flush(stdout);
-t = (time_ns() - t_start)/1.0e9;
+t = @elapsed (ANS = by(x, :id1, v1 = :v1=>sum); println(size(ANS)); flush(stdout));
 m = memory_usage();
-t_start = time_ns();
-chk = sum(ANS.v1);
-chkt = (time_ns() - t_start)/1.0e9;
+chkt = @elapsed chk = sum(ANS.v1);
 write_log(2, task, data_name, in_rows, question, size(ANS, 1), size(ANS, 2), solution, ver, git, fun, t, m, cache, make_chk(chk), chkt);
 println(first(ANS, 3));
 println(last(ANS, 3));
@@ -54,25 +44,15 @@ ANS = 0;
 
 question = "sum v1 by id1:id2"; # q2
 GC.gc();
-t_start = time_ns();
-ANS = by(x, [:id1, :id2], v1 = :v1=>sum);
-println(size(ANS)); flush(stdout);
-t = (time_ns() - t_start)/1.0e9;
+t = @elapsed (ANS = by(x, [:id1, :id2], v1 = :v1=>sum); println(size(ANS)); flush(stdout));
 m = memory_usage();
-t_start = time_ns();
-chk = sum(ANS.v1);
-chkt = (time_ns() - t_start)/1.0e9;
+chkt = @elapsed chk = sum(ANS.v1);
 write_log(1, task, data_name, in_rows, question, size(ANS, 1), size(ANS, 2), solution, ver, git, fun, t, m, cache, make_chk(chk), chkt);
 ANS = 0;
 GC.gc();
-t_start = time_ns();
-ANS = by(x, [:id1, :id2], v1 = :v1=>sum);
-println(size(ANS)); flush(stdout);
-t = (time_ns() - t_start)/1.0e9;
+t = @elapsed (ANS = by(x, [:id1, :id2], v1 = :v1=>sum); println(size(ANS)); flush(stdout));
 m = memory_usage();
-t_start = time_ns();
-chk = sum(ANS.v1);
-chkt = (time_ns() - t_start)/1.0e9;
+chkt = @elapsed chk = sum(ANS.v1);
 write_log(2, task, data_name, in_rows, question, size(ANS, 1), size(ANS, 2), solution, ver, git, fun, t, m, cache, make_chk(chk), chkt);
 println(first(ANS, 3));
 println(last(ANS, 3));
@@ -80,25 +60,15 @@ ANS = 0;
 
 question = "sum v1 mean v3 by id3"; # q3
 GC.gc();
-t_start = time_ns();
-ANS = by(x, :id3, v1 = :v1=>sum, v3 = :v3=>mean);
-println(size(ANS)); flush(stdout);
-t = (time_ns() - t_start)/1.0e9;
+t = @elapsed (ANS = by(x, :id3, v1 = :v1=>sum, v3 = :v3=>mean); println(size(ANS)); flush(stdout));
 m = memory_usage();
-t_start = time_ns();
-chk = [sum(ANS.v1), sum(ANS.v3)];
-chkt = (time_ns() - t_start)/1.0e9;
+chkt = @elapsed chk = [sum(ANS.v1), sum(ANS.v3)];
 write_log(1, task, data_name, in_rows, question, size(ANS, 1), size(ANS, 2), solution, ver, git, fun, t, m, cache, make_chk(chk), chkt);
 ANS = 0;
 GC.gc();
-t_start = time_ns();
-ANS = by(x, :id3, v1 = :v1=>sum, v3 = :v3=>mean);
-println(size(ANS)); flush(stdout);
-t = (time_ns() - t_start)/1.0e9;
+t = @elapsed (ANS = by(x, :id3, v1 = :v1=>sum, v3 = :v3=>mean); println(size(ANS)); flush(stdout));
 m = memory_usage();
-t_start = time_ns();
-chk = [sum(ANS.v1), sum(ANS.v3)];
-chkt = (time_ns() - t_start)/1.0e9;
+chkt = @elapsed chk = [sum(ANS.v1), sum(ANS.v3)];
 write_log(2, task, data_name, in_rows, question, size(ANS, 1), size(ANS, 2), solution, ver, git, fun, t, m, cache, make_chk(chk), chkt);
 println(first(ANS, 3));
 println(last(ANS, 3));
@@ -106,25 +76,16 @@ ANS = 0;
 
 question = "mean v1:v3 by id4"; # q4
 GC.gc();
-t_start = time_ns();
-ANS = by(x, :id4, v1 = :v1=>mean, v2 = :v2=>mean, v3 = :v3=>mean);
-println(size(ANS)); flush(stdout);
-t = (time_ns() - t_start)/1.0e9;
+t = @elapsed (ANS = by(x, :id4, v1 = :v1=>mean, v2 = :v2=>mean, v3 = :v3=>mean); println(size(ANS)); flush(stdout));
 m = memory_usage();
 t_start = time_ns();
-chk = [sum(ANS.v1), sum(ANS.v2), sum(ANS.v3)];
-chkt = (time_ns() - t_start)/1.0e9;
+chkt = @elapsed chk = [sum(ANS.v1), sum(ANS.v2), sum(ANS.v3)];
 write_log(1, task, data_name, in_rows, question, size(ANS, 1), size(ANS, 2), solution, ver, git, fun, t, m, cache, make_chk(chk), chkt);
 ANS = 0;
 GC.gc();
-t_start = time_ns();
-ANS = by(x, :id4, v1 = :v1=>mean, v2 = :v2=>mean, v3 = :v3=>mean);
-println(size(ANS)); flush(stdout);
-t = (time_ns() - t_start)/1.0e9;
+t = @elapsed (ANS = by(x, :id4, v1 = :v1=>mean, v2 = :v2=>mean, v3 = :v3=>mean); println(size(ANS)); flush(stdout));
 m = memory_usage();
-t_start = time_ns();
-chk = [sum(ANS.v1), sum(ANS.v2), sum(ANS.v3)];
-chkt = (time_ns() - t_start)/1.0e9;
+chkt = @elapsed chk = [sum(ANS.v1), sum(ANS.v2), sum(ANS.v3)];
 write_log(2, task, data_name, in_rows, question, size(ANS, 1), size(ANS, 2), solution, ver, git, fun, t, m, cache, make_chk(chk), chkt);
 println(first(ANS, 3));
 println(last(ANS, 3));
@@ -132,54 +93,98 @@ ANS = 0;
 
 question = "sum v1:v3 by id6"; # q5
 GC.gc();
-t_start = time_ns();
-ANS = by(x, :id6, v1 = :v1=>sum, v2 = :v2=>sum, v3 = :v3=>sum);
-println(size(ANS)); flush(stdout);
-t = (time_ns() - t_start)/1.0e9;
+t = @elapsed (ANS = by(x, :id6, v1 = :v1=>sum, v2 = :v2=>sum, v3 = :v3=>sum); println(size(ANS)); flush(stdout));
 m = memory_usage();
-t_start = time_ns();
-chk = [sum(ANS.v1), sum(ANS.v2), sum(ANS.v3)];
-chkt = (time_ns() - t_start)/1.0e9;
+chkt = @elapsed chk = [sum(ANS.v1), sum(ANS.v2), sum(ANS.v3)];
 write_log(1, task, data_name, in_rows, question, size(ANS, 1), size(ANS, 2), solution, ver, git, fun, t, m, cache, make_chk(chk), chkt);
 ANS = 0;
 GC.gc();
-t_start = time_ns();
-ANS = by(x, :id6, v1 = :v1=>sum, v2 = :v2=>sum, v3 = :v3=>sum);
-println(size(ANS)); flush(stdout);
-t = (time_ns() - t_start)/1.0e9;
+t = @elapsed (ANS = by(x, :id6, v1 = :v1=>sum, v2 = :v2=>sum, v3 = :v3=>sum); println(size(ANS)); flush(stdout));
 m = memory_usage();
-t_start = time_ns();
-chk = [sum(ANS.v1), sum(ANS.v2), sum(ANS.v3)];
-chkt = (time_ns() - t_start)/1.0e9;
+chkt = @elapsed chk = [sum(ANS.v1), sum(ANS.v2), sum(ANS.v3)];
 write_log(2, task, data_name, in_rows, question, size(ANS, 1), size(ANS, 2), solution, ver, git, fun, t, m, cache, make_chk(chk), chkt);
 println(first(ANS, 3));
 println(last(ANS, 3));
 ANS = 0;
 
-# question = "sum v3 count by id1:id6"; # q6
-# GC.gc();
-# t_start = time_ns();
-# ANS = by(x, [:id1, :id2, :id3, :id4, :id5, :id6], v3 = :v3=>sum, count = :v3=>length);
-# println(size(ANS)); flush(stdout);
-# t = (time_ns() - t_start)/1.0e9;
-# m = memory_usage();
-# t_start = time_ns();
-# chk = [sum(ANS.v3), sum(ANS.count)];
-# chkt = (time_ns() - t_start)/1.0e9;
-# write_log(1, task, data_name, in_rows, question, size(ANS, 1), size(ANS, 2), solution, ver, git, fun, t, m, cache, make_chk(chk), chkt);
-# ANS = 0;
-# GC.gc();
-# t_start = time_ns();
-# ANS = by(x, [:id1, :id2, :id3, :id4, :id5, :id6], v3 = :v3=>sum, count = :v3=>length);
-# println(size(ANS)); flush(stdout);
-# t = (time_ns() - t_start)/1.0e9;
-# m = memory_usage();
-# t_start = time_ns();
-# chk = [sum(ANS.v3), sum(ANS.count)];
-# chkt = (time_ns() - t_start)/1.0e9;
-# write_log(2, task, data_name, in_rows, question, size(ANS, 1), size(ANS, 2), solution, ver, git, fun, t, m, cache, make_chk(chk), chkt);
-# println(first(ANS, 3));
-# println(last(ANS, 3));
-# ANS = 0;
+question = "median v3 sd v3 by id2 id4"; # q6
+GC.gc();
+t = @elapsed (ANS = by(x, [:id2, :id4], median_v3 = :v3=>median, sd_v3 = :v3=>std); println(size(ANS)); flush(stdout));
+m = memory_usage();
+chkt = @elapsed chk = [sum(ANS.median_v3), sum(ANS.sd_v3)];
+write_log(1, task, data_name, in_rows, question, size(ANS, 1), size(ANS, 2), solution, ver, git, fun, t, m, cache, make_chk(chk), chkt);
+ANS = 0;
+GC.gc();
+t = @elapsed (ANS = by(x, [:id2, :id4], median_v3 = :v3=>median, sd_v3 = :v3=>std); println(size(ANS)); flush(stdout));
+m = memory_usage();
+chkt = @elapsed chk = [sum(ANS.median_v3), sum(ANS.sd_v3)];;
+write_log(2, task, data_name, in_rows, question, size(ANS, 1), size(ANS, 2), solution, ver, git, fun, t, m, cache, make_chk(chk), chkt);
+println(first(ANS, 3));
+println(last(ANS, 3));
+ANS = 0;
+
+#question = "max v1 - min v2 by id2 id4"; # q7
+#GC.gc();
+#t = @elapsed (ANS = by(x, [:id2, :id4], x->(range_v1_v2=max(x.v1)-min(x.v2))); println(size(ANS)); flush(stdout));
+#m = memory_usage();
+#chkt = @elapsed chk = sum(ANS.range_v1_v2);
+#write_log(1, task, data_name, in_rows, question, size(ANS, 1), size(ANS, 2), solution, ver, git, fun, t, m, cache, make_chk(chk), chkt);
+#ANS = 0;
+#GC.gc();
+#t = @elapsed (ANS = by(x, [:id2, :id4], x->(range_v1_v2=max(x.v1)-min(x.v2))); println(size(ANS)); flush(stdout));
+#m = memory_usage();
+#chkt = @elapsed chk = sum(ANS.range_v1_v2);
+#write_log(2, task, data_name, in_rows, question, size(ANS, 1), size(ANS, 2), solution, ver, git, fun, t, m, cache, make_chk(chk), chkt);
+#println(first(ANS, 3));
+#println(last(ANS, 3));
+#ANS = 0;
+
+question = "largest two v3 by id2 id4"; # q8
+GC.gc();
+t = @elapsed (ANS = by(sort(x[[:id2, :id4, :v3]], :v3, rev=true), [:id2, :id4], x->(largest2_v3=first(x[[:v3]], 2).v3)); println(size(ANS)); flush(stdout)); # TODO rename x1 to largest2_v3
+m = memory_usage();
+chkt = @elapsed chk = sum(ANS.x1); # TODO rename x1 to largest2_v3
+write_log(1, task, data_name, in_rows, question, size(ANS, 1), size(ANS, 2), solution, ver, git, fun, t, m, cache, make_chk(chk), chkt);
+ANS = 0;
+GC.gc();
+t = @elapsed (ANS = by(sort(x[[:id2, :id4, :v3]], :v3, rev=true), [:id2, :id4], x->(largest2_v3=first(x[[:v3]], 2).v3)); println(size(ANS)); flush(stdout));
+m = memory_usage();
+chkt = @elapsed chk = sum(ANS.x1);
+write_log(2, task, data_name, in_rows, question, size(ANS, 1), size(ANS, 2), solution, ver, git, fun, t, m, cache, make_chk(chk), chkt);
+println(first(ANS, 3));
+println(last(ANS, 3));
+ANS = 0;
+
+question = "regression v1 v2 by id2 id4"; # q9
+GC.gc();
+t = @elapsed (ANS = by(x, [:id2, :id4], x->(r2=cor(x.v1, x.v2)^2)); println(size(ANS)); flush(stdout)); # TODO rename x1 to r2
+m = memory_usage();
+chkt = @elapsed chk = sum(ANS.x1); # TODO rename x1 to r2
+write_log(1, task, data_name, in_rows, question, size(ANS, 1), size(ANS, 2), solution, ver, git, fun, t, m, cache, make_chk(chk), chkt);
+ANS = 0;
+GC.gc();
+t = @elapsed (ANS = by(x, [:id2, :id4], x->(r2=cor(x.v1, x.v2)^2)); println(size(ANS)); flush(stdout));
+m = memory_usage();
+chkt = @elapsed chk = sum(ANS.x1);
+write_log(2, task, data_name, in_rows, question, size(ANS, 1), size(ANS, 2), solution, ver, git, fun, t, m, cache, make_chk(chk), chkt);
+println(first(ANS, 3));
+println(last(ANS, 3));
+ANS = 0;
+
+question = "sum v3 count by id1:id6"; # q10
+GC.gc();
+t = @elapsed (ANS = by(x, [:id1, :id2, :id3, :id4, :id5, :id6], v3 = :v3=>sum, count = :v3=>length); println(size(ANS)); flush(stdout));
+m = memory_usage();
+chkt = @elapsed chk = [sum(ANS.v3), sum(ANS.count)];
+write_log(1, task, data_name, in_rows, question, size(ANS, 1), size(ANS, 2), solution, ver, git, fun, t, m, cache, make_chk(chk), chkt);
+ANS = 0;
+GC.gc();
+t = @elapsed (ANS = by(x, [:id1, :id2, :id3, :id4, :id5, :id6], v3 = :v3=>sum, count = :v3=>length); println(size(ANS)); flush(stdout));
+m = memory_usage();
+chkt = @elapsed chk = [sum(ANS.v3), sum(ANS.count)];
+write_log(2, task, data_name, in_rows, question, size(ANS, 1), size(ANS, 2), solution, ver, git, fun, t, m, cache, make_chk(chk), chkt);
+println(first(ANS, 3));
+println(last(ANS, 3));
+ANS = 0;
 
 exit();
