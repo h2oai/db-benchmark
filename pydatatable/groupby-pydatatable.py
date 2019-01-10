5079e70bfc320dde198b0ee7b5564b3b8f779701
jangorecki <j.gorecki@wit.edu.pl>
2019-01-10 17:46:15 +0530

    pydt q6-q9

--- pydatatable/groupby-pydatatable.py
+++ pydatatable/groupby-pydatatable.py
@@ -6,7 +6,7 @@ import os
 import gc
 import timeit
 import datatable as dt
-from datatable import f, sum, mean, count, by
+from datatable import f, sum, mean, count, sd, by
 
 exec(open("./helpers.py").read())
 
@@ -157,30 +157,134 @@ print(ans.head(3).to_pandas())
 print(ans.tail(3).to_pandas())
 del ans
 
-# question = "sum v3 count by id1:id6" # q6
-# gc.collect()
-# t_start = timeit.default_timer()
-# ans = x[:, {"v3": sum(f.v3), "count": count()}, by(f.id1, f.id2, f.id3, f.id4, f.id5, f.id6)]
-# print(ans.shape)
-# t = timeit.default_timer() - t_start
-# m = memory_usage()
-# t_start = timeit.default_timer()
-# chk = ans[:, [sum(f.v3), sum(f.count)]]
-# chkt = timeit.default_timer() - t_start
-# write_log(task=task, data=data_name, in_rows=x.shape[0], question=question, out_rows=ans.shape[0], out_cols=ans.shape[1], solution=solution, version=ver, git=git, fun=fun, run=1, time_sec=t, mem_gb=m, cache=cache, chk=make_chk(flatten(chk.to_list())), chk_time_sec=chkt)
-# del ans
-# gc.collect()
-# t_start = timeit.default_timer()
-# ans = x[:, {"v3": sum(f.v3), "count": count()}, by(f.id1, f.id2, f.id3, f.id4, f.id5, f.id6)]
-# print(ans.shape)
-# t = timeit.default_timer() - t_start
-# m = memory_usage()
-# t_start = timeit.default_timer()
-# chk = ans[:, [sum(f.v3), sum(f.count)]]
-# chkt = timeit.default_timer() - t_start
-# write_log(task=task, data=data_name, in_rows=x.shape[0], question=question, out_rows=ans.shape[0], out_cols=ans.shape[1], solution=solution, version=ver, git=git, fun=fun, run=2, time_sec=t, mem_gb=m, cache=cache, chk=make_chk(flatten(chk.to_list())), chk_time_sec=chkt)
-# print(ans.head(3).to_pandas())
-# print(ans.tail(3).to_pandas())
-# del ans
+#question = "median v3 sd v3 by id2 id4" # q6 # not yet implemented https://github.com/h2oai/datatable/issues/1530
+#gc.collect()
+#t_start = timeit.default_timer()
+#ans = x[:, {"median_v3": median(f.v3), "sd_v3": sd(f.v3)}, by(f.id2, f.id4)]
+#print(ans.shape)
+#t = timeit.default_timer() - t_start
+#m = memory_usage()
+#t_start = timeit.default_timer()
+#chk = ans[:, [sum(f.median_v3), sum(f.sd_v3)]]
+#chkt = timeit.default_timer() - t_start
+#write_log(task=task, data=data_name, in_rows=x.shape[0], question=question, out_rows=ans.shape[0], out_cols=ans.shape[1], solution=solution, version=ver, git=git, fun=fun, run=1, time_sec=t, mem_gb=m, cache=cache, chk=make_chk(flatten(chk.to_list())), chk_time_sec=chkt)
+#del ans
+#gc.collect()
+#t_start = timeit.default_timer()
+#ans = x[:, {"median_v3": median(f.v3), "sd_v3": sd(f.v3)}, by(f.id2, f.id4)]
+#print(ans.shape)
+#t = timeit.default_timer() - t_start
+#m = memory_usage()
+#t_start = timeit.default_timer()
+#chk = ans[:, [sum(f.median_v3), sum(f.sd_v3)]]
+#chkt = timeit.default_timer() - t_start
+#write_log(task=task, data=data_name, in_rows=x.shape[0], question=question, out_rows=ans.shape[0], out_cols=ans.shape[1], solution=solution, version=ver, git=git, fun=fun, run=2, time_sec=t, mem_gb=m, cache=cache, chk=make_chk(flatten(chk.to_list())), chk_time_sec=chkt)
+#print(ans.head(3).to_pandas())
+#print(ans.tail(3).to_pandas())
+#del ans
+
+#question = "max v1 - min v2 by id2 id4" # q7 # asked on slack
+#gc.collect()
+#t_start = timeit.default_timer()
+#ans = x[:, {"range_v1_v2": max(f.v1)-min(f.v2)}, by(f.id2, f.id4)]
+#print(ans.shape)
+#t = timeit.default_timer() - t_start
+#m = memory_usage()
+#t_start = timeit.default_timer()
+#chk = ans[:, sum(f.range_v1_v2)]
+#chkt = timeit.default_timer() - t_start
+#write_log(task=task, data=data_name, in_rows=x.shape[0], question=question, out_rows=ans.shape[0], out_cols=ans.shape[1], solution=solution, version=ver, git=git, fun=fun, run=1, time_sec=t, mem_gb=m, cache=cache, chk=make_chk(flatten(chk.to_list())), chk_time_sec=chkt)
+#del ans
+#gc.collect()
+#t_start = timeit.default_timer()
+#ans = x[:, {"range_v1_v2": max(f.v1)-min(f.v2)}, by(f.id2, f.id4)]
+#print(ans.shape)
+#t = timeit.default_timer() - t_start
+#m = memory_usage()
+#t_start = timeit.default_timer()
+#chk = ans[:, sum(f.range_v1_v2)]
+#chkt = timeit.default_timer() - t_start
+#write_log(task=task, data=data_name, in_rows=x.shape[0], question=question, out_rows=ans.shape[0], out_cols=ans.shape[1], solution=solution, version=ver, git=git, fun=fun, run=2, time_sec=t, mem_gb=m, cache=cache, chk=make_chk(flatten(chk.to_list())), chk_time_sec=chkt)
+#print(ans.head(3).to_pandas())
+#print(ans.tail(3).to_pandas())
+#del ans
+
+#question = "largest two v3 by id2 id4" # q8 # https://github.com/h2oai/datatable/issues/792
+#gc.collect()
+#t_start = timeit.default_timer()
+#ans = x.sort(-f.v3)[:, {"largest2_v3": head(v3, 2)}, by(f.id2, f.id4)]
+#print(ans.shape)
+#t = timeit.default_timer() - t_start
+#m = memory_usage()
+#t_start = timeit.default_timer()
+#chk = ans[:, sum(f.largest2_v3)]
+#chkt = timeit.default_timer() - t_start
+#write_log(task=task, data=data_name, in_rows=x.shape[0], question=question, out_rows=ans.shape[0], out_cols=ans.shape[1], solution=solution, version=ver, git=git, fun=fun, run=1, time_sec=t, mem_gb=m, cache=cache, chk=make_chk(flatten(chk.to_list())), chk_time_sec=chkt)
+#del ans
+#gc.collect()
+#t_start = timeit.default_timer()
+#ans = x.sort(-f.v3)[:, {"largest2_v3": head(v3, 2)}, by(f.id2, f.id4)]
+#print(ans.shape)
+#t = timeit.default_timer() - t_start
+#m = memory_usage()
+#t_start = timeit.default_timer()
+#chk = ans[:, sum(f.largest2_v3)]
+#chkt = timeit.default_timer() - t_start
+#write_log(task=task, data=data_name, in_rows=x.shape[0], question=question, out_rows=ans.shape[0], out_cols=ans.shape[1], solution=solution, version=ver, git=git, fun=fun, run=2, time_sec=t, mem_gb=m, cache=cache, chk=make_chk(flatten(chk.to_list())), chk_time_sec=chkt)
+#print(ans.head(3).to_pandas())
+#print(ans.tail(3).to_pandas())
+#del ans
+
+#question = "regression v1 v2 by id2 id4" # q9 # cor or cov+var not yet implemeneted https://github.com/h2oai/datatable/issues/290#issuecomment-453075433
+#gc.collect()
+#t_start = timeit.default_timer()
+#ans = x[:, {"r2": cor(v1, v2)^2}, by(f.id2, f.id4)]
+#print(ans.shape)
+#t = timeit.default_timer() - t_start
+#m = memory_usage()
+#t_start = timeit.default_timer()
+#chk = ans[:, sum(f.r2)]
+#chkt = timeit.default_timer() - t_start
+#write_log(task=task, data=data_name, in_rows=x.shape[0], question=question, out_rows=ans.shape[0], out_cols=ans.shape[1], solution=solution, version=ver, git=git, fun=fun, run=1, time_sec=t, mem_gb=m, cache=cache, chk=make_chk(flatten(chk.to_list())), chk_time_sec=chkt)
+#del ans
+#gc.collect()
+#t_start = timeit.default_timer()
+#ans = x[:, {"r2": cor(v1, v2)^2}, by(f.id2, f.id4)]
+#print(ans.shape)
+#t = timeit.default_timer() - t_start
+#m = memory_usage()
+#t_start = timeit.default_timer()
+#chk = ans[:, sum(f.r2)]
+#chkt = timeit.default_timer() - t_start
+#write_log(task=task, data=data_name, in_rows=x.shape[0], question=question, out_rows=ans.shape[0], out_cols=ans.shape[1], solution=solution, version=ver, git=git, fun=fun, run=2, time_sec=t, mem_gb=m, cache=cache, chk=make_chk(flatten(chk.to_list())), chk_time_sec=chkt)
+#print(ans.head(3).to_pandas())
+#print(ans.tail(3).to_pandas())
+#del ans
+
+question = "sum v3 count by id1:id6" # q10
+gc.collect()
+t_start = timeit.default_timer()
+ans = x[:, {"v3": sum(f.v3), "count": count()}, by(f.id1, f.id2, f.id3, f.id4, f.id5, f.id6)]
+print(ans.shape)
+t = timeit.default_timer() - t_start
+m = memory_usage()
+t_start = timeit.default_timer()
+chk = ans[:, [sum(f.v3), sum(f.count)]]
+chkt = timeit.default_timer() - t_start
+write_log(task=task, data=data_name, in_rows=x.shape[0], question=question, out_rows=ans.shape[0], out_cols=ans.shape[1], solution=solution, version=ver, git=git, fun=fun, run=1, time_sec=t, mem_gb=m, cache=cache, chk=make_chk(flatten(chk.to_list())), chk_time_sec=chkt)
+del ans
+gc.collect()
+t_start = timeit.default_timer()
+ans = x[:, {"v3": sum(f.v3), "count": count()}, by(f.id1, f.id2, f.id3, f.id4, f.id5, f.id6)]
+print(ans.shape)
+t = timeit.default_timer() - t_start
+m = memory_usage()
+t_start = timeit.default_timer()
+chk = ans[:, [sum(f.v3), sum(f.count)]]
+chkt = timeit.default_timer() - t_start
+write_log(task=task, data=data_name, in_rows=x.shape[0], question=question, out_rows=ans.shape[0], out_cols=ans.shape[1], solution=solution, version=ver, git=git, fun=fun, run=2, time_sec=t, mem_gb=m, cache=cache, chk=make_chk(flatten(chk.to_list())), chk_time_sec=chkt)
+print(ans.head(3).to_pandas())
+print(ans.tail(3).to_pandas())
+del ans
 
 exit(0)
