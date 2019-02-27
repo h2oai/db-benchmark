599b020d13162094a57dcab2b42b54fc97f5ed8a
jangorecki <j.gorecki@wit.edu.pl>
2019-02-27 10:06:36 +0530

    proper rm with arg

--- clickhouse-exec.sh
+++ clickhouse-exec.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+set -e
 
-rm -f "clickhouse/log_$1_q*.csv"
+rm -f clickhouse/log_$1_q*.csv
 
 cat "clickhouse/$1-clickhouse.sql" | clickhouse-client -mn --max_memory_usage=109951162777600 --log_queries 1 --format=Pretty --output_format_pretty_max_rows 1
 
