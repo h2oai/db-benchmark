8b4010ef1f82b0d8898b778e749e652d5afa3e5b
jangorecki <j.gorecki@wit.edu.pl>
2019-07-03 11:27:28 +0200

    check versions of solutions to run

--- versions.sh
+++ versions.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 set -e
 
+source ./ch.sh
+source ./run.conf
+
 # we assume client was installed/upgraded together with server
-ch_installed && clickhouse-client --version-clean > clickhouse/VERSION && echo "" > clickhouse/REVISION
+if [[ "$RUN_SOLUTIONS" =~ "clickhouse" ]]; then ch_installed && clickhouse-client --version-clean > clickhouse/VERSION && echo "" > clickhouse/REVISION; fi;
 
-Rscript -e 'v=read.dcf(system.file(package="data.table", "DESCRIPTION"), fields=c("Version","Revision")); invisible(mapply(function(f, v) writeLines(v, file.path("datatable", f)), toupper(colnames(v)), c(v)))'
-Rscript -e 'v=read.dcf(system.file(package="dplyr", "DESCRIPTION"), fields=c("Version","RemoteSha")); colnames(v)[colnames(v)=="RemoteSha"]="Revision"; invisible(mapply(function(f, v) writeLines(v, file.path("dplyr", f)), toupper(colnames(v)), c(v)))'
+if [[ "$RUN_SOLUTIONS" =~ "datatable" ]]; then Rscript -e 'v=read.dcf(system.file(package="data.table", "DESCRIPTION"), fields=c("Version","Revision")); invisible(mapply(function(f, v) writeLines(v, file.path("datatable", f)), toupper(colnames(v)), c(v)))'; fi;
+if [[ "$RUN_SOLUTIONS" =~ "dplyr" ]]; then Rscript -e 'v=read.dcf(system.file(package="dplyr", "DESCRIPTION"), fields=c("Version","RemoteSha")); colnames(v)[colnames(v)=="RemoteSha"]="Revision"; invisible(mapply(function(f, v) writeLines(v, file.path("dplyr", f)), toupper(colnames(v)), c(v)))'; fi;
 
-julia -q -e 'using Pkg; include("$(pwd())/helpers.jl"); pkgmeta = getpkgmeta("DataFrames"); f=open("juliadf/VERSION","w"); write(f, string(pkgmeta["version"])); f=open("juliadf/REVISION","w"); write(f, string(pkgmeta["git-tree-sha1"]));' > /dev/null
+if [[ "$RUN_SOLUTIONS" =~ "juliadf" ]]; then julia -q -e 'using Pkg; include("$(pwd())/helpers.jl"); pkgmeta = getpkgmeta("DataFrames"); f=open("juliadf/VERSION","w"); write(f, string(pkgmeta["version"])); f=open("juliadf/REVISION","w"); write(f, string(pkgmeta["git-tree-sha1"]));' > /dev/null; fi;
 
-source ./dask/py-dask/bin/activate
-python -c 'import dask as dk; open("dask/VERSION","w").write(dk.__version__); open("dask/REVISION","w").write(dk.__git_revision__);' > /dev/null
-source ./modin/py-modin/bin/activate
-python -c 'import modin as modin; open("modin/VERSION","w").write(modin.__version__); open("modin/REVISION","w").write("");' > /dev/null
-source ./pandas/py-pandas/bin/activate
-python -c 'import pandas as pd; open("pandas/VERSION","w").write(pd.__version__); open("pandas/REVISION","w").write(pd.__git_version__);' > /dev/null 2>&1
-source ./pydatatable/py-pydatatable/bin/activate
-python -c 'import datatable as dt; open("pydatatable/VERSION","w").write(dt.__version__); open("pydatatable/REVISION","w").write(dt.__git_revision__);' > /dev/null
-source ./spark/py-spark/bin/activate
-python -c 'import pyspark; open("spark/VERSION","w").write(pyspark.__version__); open("spark/REVISION","w").write("");' > /dev/null
+if [[ "$RUN_SOLUTIONS" =~ "dask" ]]; then source ./dask/py-dask/bin/activate && python -c 'import dask as dk; open("dask/VERSION","w").write(dk.__version__); open("dask/REVISION","w").write(dk.__git_revision__);' > /dev/null; fi;
+if [[ "$RUN_SOLUTIONS" =~ "modin" ]]; then source ./modin/py-modin/bin/activate && python -c 'import modin as modin; open("modin/VERSION","w").write(modin.__version__); open("modin/REVISION","w").write("");' > /dev/null; fi;
+if [[ "$RUN_SOLUTIONS" =~ "pandas" ]]; then source ./pandas/py-pandas/bin/activate && python -c 'import pandas as pd; open("pandas/VERSION","w").write(pd.__version__); open("pandas/REVISION","w").write(pd.__git_version__);' > /dev/null 2>&1; fi;
+if [[ "$RUN_SOLUTIONS" =~ "pydatatable" ]]; then source ./pydatatable/py-pydatatable/bin/activate && python -c 'import datatable as dt; open("pydatatable/VERSION","w").write(dt.__version__); open("pydatatable/REVISION","w").write(dt.__git_revision__);' > /dev/null; fi;
+if [[ "$RUN_SOLUTIONS" =~ "spark" ]]; then source ./spark/py-spark/bin/activate && python -c 'import pyspark; open("spark/VERSION","w").write(pyspark.__version__); open("spark/REVISION","w").write("");' > /dev/null; fi;
