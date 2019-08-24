fcc6caa3468fb319f48ff9488d1a69575ff407c7
jangorecki <j.gorecki@wit.edu.pl>
2019-08-24 15:06:45 +0200

    conda source req in ver cudf script

--- cudf/ver-cudf.sh
+++ cudf/ver-cudf.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+source ~/anaconda3/etc/profile.d/conda.sh # conda required for cudf
+
 conda activate cudf
 python -c 'import cudf; open("cudf/VERSION","w").write(cudf.__version__.split("+", 1)[0]); open("cudf/REVISION","w").write("");' > /dev/null
 conda deactivate
