5e79ae9e3b1a37f6da413bb8ead4139ad126a026
jangorecki <j.gorecki@wit.edu.pl>
2019-10-15 11:27:04 +0200

    fix pandas chk spotted thanks to answers validations

--- pandas/join-pandas.py
+++ pandas/join-pandas.py
@@ -123,7 +123,7 @@ print(ans.shape, flush=True)
 t = timeit.default_timer() - t_start
 m = memory_usage()
 t_start = timeit.default_timer()
-chk = [ans['v1'].sum(), ans['v1'].sum()]
+chk = [ans['v1'].sum(), ans['v2'].sum()]
 chkt = timeit.default_timer() - t_start
 write_log(task=task, data=data_name, in_rows=x.shape[0], question=question, out_rows=ans.shape[0], out_cols=ans.shape[1], solution=solution, version=ver, git=git, fun=fun, run=1, time_sec=t, mem_gb=m, cache=cache, chk=make_chk(chk), chk_time_sec=chkt)
 del ans
