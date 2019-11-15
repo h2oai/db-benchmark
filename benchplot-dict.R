d6b3dc50bb19ea01427cf9a26dafb971cd1630c0
jangorecki <j.gorecki@wit.edu.pl>
2019-11-15 09:56:39 +0530

    pydt now handles big-to-big join having own deep copy method

--- benchplot-dict.R
+++ benchplot-dict.R
@@ -265,7 +265,6 @@ join.data.exceptions = {list(                                                   
     "out of memory" = c("J1_1e9_NA_0_0")                                                  # read_csv
   )},
   "pydatatable" = {list(
-    "out of memory" = c("J1_1e9_NA_0_0")                                                  # q5
   )},
   "spark" = {list(
     "out of memory" = c("J1_1e9_NA_0_0")                                                  # read_csv
