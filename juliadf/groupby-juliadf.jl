15935c385e8be825673792d58b2e9ea1776655b5
jangorecki <j.gorecki@wit.edu.pl>
2019-11-16 10:53:58 +0530

    juliadf advanced groupby questions #117

--- juliadf/groupby-juliadf.jl
+++ juliadf/groupby-juliadf.jl
@@ -5,6 +5,7 @@ print("# groupby-juliadf.jl\n"); flush(stdout);
 using DataFrames;
 using CSV;
 using Statistics; # mean function
+using Printf;
 
 include("$(pwd())/helpers.jl");
 
@@ -24,6 +25,7 @@ x = DataFrame(CSV.File(src_grp, pool=true));
 in_rows = size(x, 1);
 println(in_rows); flush(stdout);
 
+task_init = time();
 print("grouping...\n"); flush(stdout);
 
 question = "sum v1 by id1"; # q1
@@ -107,15 +109,15 @@ println(first(ANS, 3));
 println(last(ANS, 3));
 ANS = 0;
 
-question = "median v3 sd v3 by id2 id4"; # q6
+question = "median v3 sd v3 by id4 id5"; # q6
 GC.gc();
-t = @elapsed (ANS = by(x, [:id2, :id4], median_v3 = :v3 => median, sd_v3 = :v3 => std); println(size(ANS)); flush(stdout));
+t = @elapsed (ANS = by(x, [:id4, :id5], median_v3 = :v3 => median, sd_v3 = :v3 => std); println(size(ANS)); flush(stdout));
 m = memory_usage();
 chkt = @elapsed chk = [sum(ANS.median_v3), sum(ANS.sd_v3)];
 write_log(1, task, data_name, in_rows, question, size(ANS, 1), size(ANS, 2), solution, ver, git, fun, t, m, cache, make_chk(chk), chkt);
 ANS = 0;
 GC.gc();
-t = @elapsed (ANS = by(x, [:id2, :id4], median_v3 = :v3 => median, sd_v3 = :v3 => std); println(size(ANS)); flush(stdout));
+t = @elapsed (ANS = by(x, [:id4, :id5], median_v3 = :v3 => median, sd_v3 = :v3 => std); println(size(ANS)); flush(stdout));
 m = memory_usage();
 chkt = @elapsed chk = [sum(ANS.median_v3), sum(ANS.sd_v3)];;
 write_log(2, task, data_name, in_rows, question, size(ANS, 1), size(ANS, 2), solution, ver, git, fun, t, m, cache, make_chk(chk), chkt);
@@ -123,15 +125,15 @@ println(first(ANS, 3));
 println(last(ANS, 3));
 ANS = 0;
 
-question = "max v1 - min v2 by id2 id4"; # q7
+question = "max v1 - min v2 by id3"; # q7
 GC.gc();
-t = @elapsed (ANS = by(x, [:id2, :id4], range_v1_v2 = [:v1, :v2] => x -> maximum(skipmissing(x.v1))-minimum(skipmissing(x.v2))); println(size(ANS)); flush(stdout));
+t = @elapsed (ANS = by(x, [:id3], range_v1_v2 = [:v1, :v2] => x -> maximum(skipmissing(x.v1))-minimum(skipmissing(x.v2))); println(size(ANS)); flush(stdout));
 m = memory_usage();
 chkt = @elapsed chk = sum(ANS.range_v1_v2);
 write_log(1, task, data_name, in_rows, question, size(ANS, 1), size(ANS, 2), solution, ver, git, fun, t, m, cache, make_chk(chk), chkt);
 ANS = 0;
 GC.gc();
-t = @elapsed (ANS = by(x, [:id2, :id4], range_v1_v2 = [:v1, :v2] => x -> maximum(skipmissing(x.v1))-minimum(skipmissing(x.v2))); println(size(ANS)); flush(stdout));
+t = @elapsed (ANS = by(x, [:id3], range_v1_v2 = [:v1, :v2] => x -> maximum(skipmissing(x.v1))-minimum(skipmissing(x.v2))); println(size(ANS)); flush(stdout));
 m = memory_usage();
 chkt = @elapsed chk = sum(ANS.range_v1_v2);
 write_log(2, task, data_name, in_rows, question, size(ANS, 1), size(ANS, 2), solution, ver, git, fun, t, m, cache, make_chk(chk), chkt);
@@ -139,15 +141,15 @@ println(first(ANS, 3));
 println(last(ANS, 3));
 ANS = 0;
 
-question = "largest two v3 by id2 id4"; # q8
+question = "largest two v3 by id6"; # q8
 GC.gc();
-t = @elapsed (ANS = by(x, [:id2, :id4], largest2_v3 = :v3 => x -> partialsort(x, 1:2, rev=true)); println(size(ANS)); flush(stdout));
+t = @elapsed (ANS = by(x, [:id6], largest2_v3 = :v3 => x -> partialsort(x, 1:2, rev=true)); println(size(ANS)); flush(stdout));
 m = memory_usage();
 chkt = @elapsed chk = sum(ANS.largest2_v3);
 write_log(1, task, data_name, in_rows, question, size(ANS, 1), size(ANS, 2), solution, ver, git, fun, t, m, cache, make_chk(chk), chkt);
 ANS = 0;
 GC.gc();
-t = @elapsed (ANS = by(x, [:id2, :id4], largest2_v3 = :v3 => x -> partialsort(x, 1:2, rev=true)); println(size(ANS)); flush(stdout));
+t = @elapsed (ANS = by(x, [:id6], largest2_v3 = :v3 => x -> partialsort(x, 1:2, rev=true)); println(size(ANS)); flush(stdout));
 m = memory_usage();
 chkt = @elapsed chk = sum(ANS.largest2_v3);
 write_log(2, task, data_name, in_rows, question, size(ANS, 1), size(ANS, 2), solution, ver, git, fun, t, m, cache, make_chk(chk), chkt);
@@ -186,5 +188,7 @@ write_log(2, task, data_name, in_rows, question, size(ANS, 1), size(ANS, 2), sol
 println(first(ANS, 3));
 println(last(ANS, 3));
 ANS = 0;
+
+print(@sprintf "grouping finished, took %.0fs\n" (time()-task_init)); flush(stdout);
 
 exit();
