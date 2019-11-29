15d14e3b5a494490ecb75e38c6fe1fef82925047
jangorecki <j.gorecki@wit.edu.pl>
2019-11-29 20:24:04 +0530

    missing path change

--- clickhouse/ver-clickhouse.sh
+++ clickhouse/ver-clickhouse.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-source ./ch.sh # clickhouse helper scripts
+source ./clickhouse/ch.sh # clickhouse helper scripts
 
 ch_installed && clickhouse-client --version-clean > clickhouse/VERSION && echo "" > clickhouse/REVISION
