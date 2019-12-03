dfdd85dcdf9a84d9ce3f299eece5676d4aecf671
jangorecki <j.gorecki@wit.edu.pl>
2019-12-03 20:12:22 +0530

    more robust adhoc queries of timings

--- _utils/tailtime.R
+++ _utils/tailtime.R
@@ -1,6 +1,6 @@
 source("./_report/report.R")
 
-download.dbb = function(file=c("logs.csv","time.csv"), from="https://h2oai.github.io/db-benchmark") {
+download.time = function(file=c("logs.csv","time.csv"), from="https://h2oai.github.io/db-benchmark") {
   stopifnot(is.character(file), is.character(from), length(file)>=1L, length(from)==1L, !is.na(file), !is.na(from))
   if (all(file.exists(file))) {
     md5file = paste(file, "md5", sep=".")
@@ -18,18 +18,49 @@ download.dbb = function(file=c("logs.csv","time.csv"), from="https://h2oai.githu
   return(invisible(NULL))
 }
 
-tailtime = function(solution, task, n=2L) {
+drop.data.table = function(x, cols) {
+  ans = data.table:::shallow(x)
+  un = sapply(cols, function(col) uniqueN(x[[col]]))
+  rm = names(un)[un <= 1L]
+  if (length(rm)) set(ans, NULL, rm, NULL) # Rdatatable/data.table#4086
+  ans
+}
+
+tail.time = function(solution, task, n=2L, i=seq_len(n), drop=TRUE) {
+  stopifnot(length(solution)==1L, length(task)==1L, length(n)==1L, n>0L, length(i)>=1L, all(i>=0L))
+  if (!missing(n) && !missing(i)) stop("only 'n' or 'i' argument should be used, not both")
   ld = time_logs()
   s = solution
   t = task
   ld = ld[solution==s & task==t]
-  nbatch = tail(unique(ld$batch), n=n)
-  dcast(
-    ld[batch%in%nbatch],
+  ub = unique(ld$batch)
+  i = i[i <= length(ub)] # there might be only N unq batches but N+1 requested
+  if (!length(i)) stop("there are not enough registered runs for this solution and requested recent timings")
+  b = rev(ub)[i]
+  ans = dcast(
+    ld[batch%in%b],
     in_rows + knasorted + question_group + question ~ paste(format(as.POSIXct(as.numeric(batch), origin="1970-01-01"), "%Y%m%d"), substr(git, 1, 7), sep="_"),
-    value.var="time_sec_1"
+    value.var = "time_sec_1"
   )
+  if (drop) ans = drop.data.table(ans, cols=c("in_rows","knasorted","question_group","question"))
+  ans
 }
 
-download.dbb()
-tailtime("data.table", "groupby")
+compare.time = function(solutions, task, drop=TRUE) {
+  stopifnot(length(solutions)>=1L, length(task)==1L)
+  ld = time_logs()
+  t = task
+  ans = dcast(
+    ld[script_recent==TRUE & solution%in%solutions & task==t],
+    in_rows + knasorted + question_group + question ~ solution,
+    value.var = "time_sec_1"
+  )
+  if (drop) ans = drop.data.table(ans, cols=c("in_rows","knasorted","question_group","question"))
+  ans
+}
+
+if (!identical(Sys.info()[["user"]], "jan")) { # one wants to grab recently timings, but "jan" might have used scp to get unpublished timings
+  download.time()
+}
+tail.time("data.table", "groupby", i=c(1L, 6L, 11L)) # every fifth run
+compare.time(c("data.table","spark","pydatatable"), "join")
