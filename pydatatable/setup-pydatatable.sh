74a8be37b1b7aa7cb131b5dd643e74035b00b2f0
jangorecki <j.gorecki@wit.edu.pl>
2020-02-23 12:25:15 +0530

    easier pyDT setup

--- pydatatable/setup-pydatatable.sh
+++ pydatatable/setup-pydatatable.sh
@@ -3,18 +3,11 @@ set -e
 
 # install dependencies
 sudo apt-get update
-sudo apt-get install python3.6-dev gcc-8-multilib # or other multilib, depends on your gcc version
+sudo apt-get install python3.6-dev
 
 virtualenv pydatatable/py-pydatatable --python=/usr/bin/python3.6
 source pydatatable/py-pydatatable/bin/activate
 
-# install llvm to build pydatatable
-wget https://releases.llvm.org/6.0.1/clang+llvm-6.0.1-x86_64-linux-gnu-ubuntu-16.04.tar.xz
-tar -xvf clang+llvm-6.0.1-x86_64-linux-gnu-ubuntu-16.04.tar.xz
-sudo mv clang+llvm-6.0.1-x86_64-linux-gnu-ubuntu-16.04 /opt
-rm clang+llvm-6.0.1-x86_64-linux-gnu-ubuntu-16.04.tar.xz
-
-python -m pip install --upgrade llvmlite
 python -m pip install --upgrade psutil
 
 # build
