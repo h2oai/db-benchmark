105f37fcdad6eecdf5fd3e4b5969c63ebbd298cc
jangorecki <j.gorecki@wit.edu.pl>
2020-06-19 23:11:51 +0100

    CH try to solve q10

--- clickhouse/exec.sh
+++ clickhouse/exec.sh
@@ -20,6 +20,7 @@ ch_active || exit 1
 CH_MEM=107374182400 # 100GB ## old value 128849018880 # 120GB ## now set to 96GB after cache=1 to in-memory temp tables because there was not enough mem for R to parse timings
 CH_EXT_GRP_BY=53687091200 # twice less than CH_MEM #96
 CH_EXT_SORT=53687091200
+clickhouse-client --query="DROP TABLE IF EXISTS ans"
 clickhouse-client --query="TRUNCATE TABLE $2"
 clickhouse-client --max_memory_usage=$CH_MEM --query="INSERT INTO $2 FORMAT CSVWithNames" < "data/$2.csv"
 # confirm all data loaded yandex/ClickHouse#4463
@@ -38,13 +39,16 @@ echo "# clickhouse/exec.sh: data loaded, logs truncated, $1-$2 script prepared, 
 cat "clickhouse/$1-clickhouse.sql" | clickhouse-client -mn --max_memory_usage=$CH_MEM --max_bytes_before_external_group_by=$CH_EXT_GRP_BY --max_bytes_before_external_sort=$CH_EXT_SORT --receive_timeout=10800 --format=Pretty --output_format_pretty_max_rows 1 && echo "# clickhouse/exec.sh: benchmark sql script finished" || echo "# clickhouse/exec.sh: benchmark sql script for $2 terminated with error"
 
 # need to wait in case if server crashed to release memory
-sleep 120
-
-# parse timings from clickhouse/log/[task]_[data_name]_q[i]_r[j].csv
-Rscript clickhouse/clickhouse-parse-log.R "$1" "$2"
+sleep 90
 
 # cleanup data
-ch_active && echo "# clickhouse/exec.sh: finishing, truncating table $2" && clickhouse-client --query="TRUNCATE TABLE $2" || echo "# clickhouse/exec.sh: finishing, clickhouse server down, possibly crashed, could not truncate table $2"
+ch_active && echo "# clickhouse/exec.sh: finishing, truncating table $2" && clickhouse-client --query="DROP TABLE IF EXISTS ans" && clickhouse-client --query="TRUNCATE TABLE $2" || echo "# clickhouse/exec.sh: finishing, clickhouse server down, possibly crashed, could not truncate table $2"
 
 # stop server
 ch_stop && echo "# clickhouse/exec.sh: stopping server finished" || echo "# clickhouse/exec.sh: stopping server failed"
+
+# wait for memory
+sleep 30
+
+# parse timings from clickhouse/log/[task]_[data_name]_q[i]_r[j].csv
+Rscript clickhouse/clickhouse-parse-log.R "$1" "$2"
