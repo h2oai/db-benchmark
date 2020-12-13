27663759e56388be45fa30f1d7ad20401e848df3
jangorecki <j.gorecki@wit.edu.pl>
2020-12-13 13:22:38 +0200

    fix order of CH logs where q10 was before q1

--- clickhouse/clickhouse-parse-log.R
+++ clickhouse/clickhouse-parse-log.R
@@ -9,11 +9,26 @@ task = args[1L]
 data_name = args[2L]
 
 library(data.table)
-
+# sort files according to question and run
+sort_q_r = function(f) {
+  tmp = strsplit(tools::file_path_sans_ext(basename(f)), "_", fixed=TRUE)
+  if (length(len<-unique(lengths(tmp)))!=1L)
+    stop("files names for some of logs differs in number of underscores, it should be clickhouse/log/[task]_[data_name]_q[i]_r[j].csv")
+  stopifnot(len>1L)
+  qr = rbindlist(lapply(lapply(tmp, `[`, c(len-1L,len)), function(x) {
+    stopifnot(substr(x[1L], 1L, 1L)=="q", substr(x[2L], 1L, 1L)=="r")
+    list(q=as.integer(substr(x[1L], 2L, nchar(x[1L]))), r=as.integer(substr(x[2L], 2L, nchar(x[2L]))))
+  }))
+  o = data.table:::forderv(qr) ## https://github.com/Rdatatable/data.table/issues/3447
+  if (!length(o)) f else f[o]
+}
 fcsv = list.files("clickhouse/log", sprintf("^%s_%s_q.*\\.csv$", task, data_name), full.names=TRUE)
-if (!length(fcsv)) stop("no log files produced, did you run clickhouse sql script that will output such to clickhouse/log/[task]_[data_name]_q[i]_r[j].csv")
+if (!length(fcsv))
+  stop("no log files produced, did you run clickhouse sql script that will output such to clickhouse/log/[task]_[data_name]_q[i]_r[j].csv")
+fcsv = sort_q_r(fcsv)
 d = rbindlist(lapply(fcsv, fread, na.strings="\\N")) # fill=TRUE for debugging type column in some queries
-if (!nrow(d)) stop("timing log files empty")
+if (!nrow(d))
+  stop("timing log files empty")
 stopifnot(all(d$task==task), all(d$data_name==data_name))
 .in_rows = strsplit(data_name, "_", fixed=TRUE)[[1L]][[2L]] ## taken from data_name because for join CH will sum in rows from both tables
 d[,
