40d2b9a6d5793ea7814dd3dd8104de2e01780334
jangorecki <j.gorecki@wit.edu.pl>
2020-12-14 12:26:48 +0200

    minor code formatting

--- juliadf/groupby-juliadf.jl
+++ juliadf/groupby-juliadf.jl
@@ -26,8 +26,10 @@ src_grp = string("data/", data_name, ".csv");
 println(string("loading dataset ", data_name)); flush(stdout);
 
 # Types are provided explicitly only to reduce memory use when parsing
-x = DataFrame(CSV.File(src_grp, pool=true,
-                       types=[PooledString, PooledString, PooledString, Int, Int, Int, Int, Int, Float64]));
+x = DataFrame(CSV.File(
+  src_grp, pool=true,
+  types=[PooledString, PooledString, PooledString, Int, Int, Int, Int, Int, Float64]
+));
 in_rows = size(x, 1);
 println(in_rows); flush(stdout);
 
