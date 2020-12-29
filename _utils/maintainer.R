8fbdd613208ccf15bce5f514b25082dd06a1e0b6
jangorecki <j.gorecki@wit.edu.pl>
2020-12-29 20:18:29 +0200

    better timeleft

--- _utils/maintainer.R
+++ _utils/maintainer.R
@@ -10,7 +10,7 @@ timeleft = function() {
   stopifnot(this$action=="start")
   l = l[-.N][action!="skip", data.table::dcast(.SD, solution+task+data+batch~action, value.var="timestamp")]
   took = l[this, on=.(solution, task, data), nomatch=NULL, finish[.N]-start[.N]]
-  if (is.na(took)) {
+  if (!length(took) || is.na(took)) {
     this[, cat(sprintf("%s %s %s is running for the first time so it is unknown how much it will run\n", solution, task, data))]
     quit("no")
   }
