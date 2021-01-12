6e00a1ca9a84083ce8976a60d1d6684ffd860793
jangorecki <j.gorecki@wit.edu.pl>
2021-01-12 16:50:36 +0200

    proper module name

--- polars/ver-polars.sh
+++ polars/ver-polars.sh
@@ -2,4 +2,4 @@
 set -e
 
 source ./polars/py-polars/bin/activate
-python -c 'import polars as pl; open("polars/VERSION","w").write(pl.__version__); open("polars/REVISION","w").write("");' > /dev/null
+python -c 'import pypolars as pl; open("polars/VERSION","w").write(pl.__version__); open("polars/REVISION","w").write("");' > /dev/null
