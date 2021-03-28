09b50360abfbf04ab1531598f442d7c004df0462
Bogumił Kamiński <bkamins@sgh.waw.pl>
2021-03-28 01:11:43 +0100

    launch julia in mulit-threaded mode

--- juliadf/ver-juliadf.sh
+++ juliadf/ver-juliadf.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-julia -q -e 'include("$(pwd())/_helpers/helpers.jl"); pkgmeta = getpkgmeta("DataFrames"); f=open("juliadf/VERSION","w"); write(f, string(pkgmeta["version"])); f=open("juliadf/REVISION","w"); write(f, string(pkgmeta["git-tree-sha1"]));' > /dev/null
+julia -t auto -q -e 'include("$(pwd())/_helpers/helpers.jl"); pkgmeta = getpkgmeta("DataFrames"); f=open("juliadf/VERSION","w"); write(f, string(pkgmeta["version"])); f=open("juliadf/REVISION","w"); write(f, string(pkgmeta["git-tree-sha1"]));' > /dev/null
