a720d15d188ebab263f70d9580707e2ea837b967
Bogumił Kamiński <bkamins@sgh.waw.pl>
2021-04-22 23:56:03 +0200

    Update groupby-juliadf.jl

--- juliadf/groupby-juliadf.jl
+++ juliadf/groupby-juliadf.jl
@@ -26,10 +26,7 @@ src_grp = string("data/", data_name, ".csv");
 println(string("loading dataset ", data_name)); flush(stdout);
 
 # Types are provided explicitly only to reduce memory use when parsing
-x = DataFrame(CSV.File(
-  src_grp, pool=true,
-  types=[PooledString, PooledString, PooledString, Int, Int, Int, Int, Int, Float64]
-));
+x = CSV.read(src_grp, DataFrame);
 in_rows = size(x, 1);
 println(in_rows); flush(stdout);
 
