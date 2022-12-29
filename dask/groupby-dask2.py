430fe5bc995d6ea351e32f3be64938500fae7f69
Tmonster <tom@ebergen.com>
2022-12-29 23:53:45 +0000

    making sure the files are run as python3 files

--- dask/groupby-dask2.py
+++ dask/groupby-dask2.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 print("# groupby-dask.py", flush=True)
 
--- spark/join-spark.py
+++ spark/join-spark.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 print("# join-spark.py", flush=True)
 
