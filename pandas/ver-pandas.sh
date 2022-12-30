debda8b7fdc19fa0e6ac70a519ed270e0e3a5319
Tmonster <tom@ebergen.com>
2022-12-30 00:31:53 +0000

    pandas needs numeric_only attribute when calling .corr

--- pandas/ver-pandas.sh
+++ pandas/ver-pandas.sh
@@ -2,4 +2,4 @@
 set -e
 
 source ./pandas/py-pandas/bin/activate
-python -c 'import pandas as pd; open("pandas/VERSION","w").write(pd.__version__); open("pandas/REVISION","w").write(pd.__git_version__);' > /dev/null
+python3 -c 'import pandas as pd; open("pandas/VERSION","w").write(pd.__version__); open("pandas/REVISION","w").write(pd.__git_version__);' > /dev/null
