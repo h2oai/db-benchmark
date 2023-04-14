82b1744643636bd531bc12a68cd61e9f3ce09a1f
Tom Ebergen <tom@ebergen.com>
2023-04-14 09:37:39 +0200

    upgrade dask scripts. remove queries that consistently throw error and warnings

--- dask/groupby-dask2.py
+++ dask/groupby-dask2.py
@@ -217,33 +217,33 @@ del ans
 #print(ans.tail(3), flush=True)
 #del ans
 
-#question = "max v1 - min v2 by id3" # q7
-#gc.collect()
-#t_start = timeit.default_timer()
-#ans = x.groupby('id3', dropna=False, observed=True).agg({'v1':'max', 'v2':'min'}).assign(range_v1_v2=lambda x: x['v1']-x['v2'])[['range_v1_v2']].compute()
-#ans.reset_index(inplace=True)
-#print(ans.shape, flush=True)
-#t = timeit.default_timer() - t_start
-#m = memory_usage()
-#t_start = timeit.default_timer()
-#chk = [ans['range_v1_v2'].sum()]
-#chkt = timeit.default_timer() - t_start
-#write_log(task=task, data=data_name, in_rows=in_rows, question=question, out_rows=ans.shape[0], out_cols=ans.shape[1], solution=solution, version=ver, git=git, fun=fun, run=1, time_sec=t, mem_gb=m, cache=cache, chk=make_chk(chk), chk_time_sec=chkt, on_disk=on_disk)
-#del ans
-#gc.collect()
-#t_start = timeit.default_timer()
-#ans = x.groupby('id3', dropna=False, observed=True).agg({'v1':'max', 'v2':'min'}).assign(range_v1_v2=lambda x: x['v1']-x['v2'])[['range_v1_v2']].compute()
-#ans.reset_index(inplace=True)
-#print(ans.shape, flush=True)
-#t = timeit.default_timer() - t_start
-#m = memory_usage()
-#t_start = timeit.default_timer()
-#chk = [ans['range_v1_v2'].sum()]
-#chkt = timeit.default_timer() - t_start
-#write_log(task=task, data=data_name, in_rows=in_rows, question=question, out_rows=ans.shape[0], out_cols=ans.shape[1], solution=solution, version=ver, git=git, fun=fun, run=2, time_sec=t, mem_gb=m, cache=cache, chk=make_chk(chk), chk_time_sec=chkt, on_disk=on_disk)
-#print(ans.head(3), flush=True)
-#print(ans.tail(3), flush=True)
-#del ans
+# question = "max v1 - min v2 by id3" # q7
+# gc.collect()
+# t_start = timeit.default_timer()
+# ans = x.groupby('id3', dropna=False, observed=True).agg({'v1':'max', 'v2':'min'}).assign(range_v1_v2=lambda x: x['v1']-x['v2'])[['range_v1_v2']].compute()
+# ans.reset_index(inplace=True)
+# print(ans.shape, flush=True)
+# t = timeit.default_timer() - t_start
+# m = memory_usage()
+# t_start = timeit.default_timer()
+# chk = [ans['range_v1_v2'].sum()]
+# chkt = timeit.default_timer() - t_start
+# write_log(task=task, data=data_name, in_rows=in_rows, question=question, out_rows=ans.shape[0], out_cols=ans.shape[1], solution=solution, version=ver, git=git, fun=fun, run=1, time_sec=t, mem_gb=m, cache=cache, chk=make_chk(chk), chk_time_sec=chkt, on_disk=on_disk)
+# del ans
+# gc.collect()
+# t_start = timeit.default_timer()
+# ans = x.groupby('id3', dropna=False, observed=True).agg({'v1':'max', 'v2':'min'}).assign(range_v1_v2=lambda x: x['v1']-x['v2'])[['range_v1_v2']].compute()
+# ans.reset_index(inplace=True)
+# print(ans.shape, flush=True)
+# t = timeit.default_timer() - t_start
+# m = memory_usage()
+# t_start = timeit.default_timer()
+# chk = [ans['range_v1_v2'].sum()]
+# chkt = timeit.default_timer() - t_start
+# write_log(task=task, data=data_name, in_rows=in_rows, question=question, out_rows=ans.shape[0], out_cols=ans.shape[1], solution=solution, version=ver, git=git, fun=fun, run=2, time_sec=t, mem_gb=m, cache=cache, chk=make_chk(chk), chk_time_sec=chkt, on_disk=on_disk)
+# print(ans.head(3), flush=True)
+# print(ans.tail(3), flush=True)
+# del ans
 
 #question = "largest two v3 by id6" # q8
 #gc.collect()
