bf7fac783e6e10f9973e51e52cdc7975746ffad3
Tom Ebergen <tom@ebergen.com>
2023-06-21 12:42:52 +0200

    updates to regression script. only run on pull request. also fix pydatatable

---
+++ pydatatable/min-setup-pydatatable.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+# install dependencies
+sudo apt-get update -qq
+sudo apt-get install -y python3.10 virtualenv
+
+virtualenv pydatatable/py-pydatatable --python=/usr/bin/python3.10
+source pydatatable/py-pydatatable/bin/activate
+
+python -m pip install --upgrade psutil
+
+# build
+deactivate
+./pydatatable/upg-pydatatable.sh
